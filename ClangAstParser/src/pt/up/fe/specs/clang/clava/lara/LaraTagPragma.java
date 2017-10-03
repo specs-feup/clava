118b24e90cc90a3f5892a406a64ae0e6fa5131bd
pedro <p.pinto@fe.up.pt>
2017-10-03 16:03:54 +0100

    [ClangAstParser] Adds parser support for tag pragmas

---
+++ ClangAstParser/src/pt/up/fe/specs/clang/clava/lara/LaraTagPragma.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2017 SPeCS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package pt.up.fe.specs.clang.clava.lara;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import pt.up.fe.specs.clava.ClavaNode;
+import pt.up.fe.specs.clava.ClavaNodeInfo;
+import pt.up.fe.specs.clava.ast.pragma.Pragma;
+
+public class LaraTagPragma extends Pragma {
+
+    private static final String LARA_TAG_PREFIX = "lara tag ";
+
+    private String tagId;
+
+    public LaraTagPragma(String tagId, ClavaNodeInfo info) {
+        this(tagId, info, Collections.emptyList());
+    }
+
+    private LaraTagPragma(String tagId, ClavaNodeInfo info, Collection<? extends ClavaNode> children) {
+        super(info, children);
+
+        this.tagId = tagId;
+    }
+
+    @Override
+    protected ClavaNode copyPrivate() {
+        return new LaraTagPragma(tagId, getInfo(), Collections.emptyList());
+    }
+
+    @Override
+    public String getFullContent() {
+        return LARA_TAG_PREFIX + tagId;
+    }
+
+    public void setFullContent(String fullContent) {
+        String newContent = fullContent;
+
+        if (newContent.startsWith(LARA_TAG_PREFIX)) {
+            newContent = newContent.substring(LARA_TAG_PREFIX.length());
+        }
+
+        tagId = newContent;
+    }
+
+    public String getTagId() {
+        return tagId;
+    }
+
+    @Override
+    public String getCode() {
+        return "#pragma " + getFullContent();
+    }
+}
--- ClangAstParser/src/pt/up/fe/specs/clang/pragma/LaraPragmaParser.java
+++ ClangAstParser/src/pt/up/fe/specs/clang/pragma/LaraPragmaParser.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2017 SPeCS.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
@@ -17,6 +17,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import pt.up.fe.specs.clang.clava.lara.LaraMarkerPragma;
+import pt.up.fe.specs.clang.clava.lara.LaraTagPragma;
 import pt.up.fe.specs.clava.ClavaNodeInfo;
 import pt.up.fe.specs.clava.ast.pragma.Pragma;
 import pt.up.fe.specs.util.stringparser.StringParser;
@@ -28,6 +29,7 @@ public class LaraPragmaParser implements PragmaParser {
     static {
         LARA_PARSERS = new HashMap<>();
         LARA_PARSERS.put("marker", LaraPragmaParser::marker);
+        LARA_PARSERS.put("tag", LaraPragmaParser::tag);
     }
 
     @Override
@@ -53,6 +55,13 @@ public class LaraPragmaParser implements PragmaParser {
         String markerId = contents.apply(StringParsers::parseWord);
 
         return new LaraMarkerPragma(markerId, info);
+    }
+
+    private static Pragma tag(StringParser contents, ClavaNodeInfo info) {
+
+        String refId = contents.apply(StringParsers::parseWord);
+
+        return new LaraTagPragma(refId, info);
     }
 
 }
