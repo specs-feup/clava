bf7b508e5a1fdca86e5933f0d532577b98b18f62
JoaoBispo <joaobispo@gmail.com>
2017-11-03 19:00:34 +0000

    [ClavaAst] Adds node ImplicitValueInitExpr

---
+++ ClavaAst/src/pt/up/fe/specs/clava/ast/expr/ImplicitValueInitExpr.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2016 SPeCS.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package pt.up.fe.specs.clava.ast.expr;
+
+import java.util.Collections;
+import java.util.List;
+
+import pt.up.fe.specs.clava.ClavaNode;
+import pt.up.fe.specs.clava.ClavaNodeInfo;
+import pt.up.fe.specs.clava.ast.expr.data.ExprData;
+
+/**
+ * Represents an implicit type conversions which has no direct representation in the original source code.
+ * 
+ * @author JoaoBispo
+ *
+ */
+public class ImplicitValueInitExpr extends Expr {
+
+    public ImplicitValueInitExpr(ExprData exprData, ClavaNodeInfo info) {
+        this(exprData, info, Collections.emptyList());
+    }
+
+    /**
+     * Constructor for node copy.
+     * 
+     * @param castKind
+     * @param destinationTypes
+     * @param location
+     */
+    private ImplicitValueInitExpr(ExprData exprData, ClavaNodeInfo info,
+            List<? extends ClavaNode> children) {
+
+        super(exprData, info, children);
+    }
+
+    @Override
+    protected ClavaNode copyPrivate() {
+        return new ImplicitValueInitExpr(getExprData(), getInfo(), Collections.emptyList());
+    }
+
+    @Override
+    public String toContentString() {
+        return super.toContentString() + " (type:" + getExprType().getCode() + ")";
+    }
+
+}
