2fbf4e6ba9bef9b840990d3c8f46fd41088362f4
JoaoBispo <joaobispo@gmail.com>
2017-11-18 17:10:43 +0000

    [ClavaWeaver] Moves functionality previously in CxxOmp to OmpClauses

--- ClavaWeaver/src/pt/up/fe/specs/clava/weaver/joinpoints/CxxOmp.java
+++ ClavaWeaver/src/pt/up/fe/specs/clava/weaver/joinpoints/CxxOmp.java
@@ -13,25 +13,13 @@
 
 package pt.up.fe.specs.clava.weaver.joinpoints;
 
-import static pt.up.fe.specs.clava.ast.omp.clauses.OmpClauseKind.*;
-
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
-import pt.up.fe.specs.clava.ClavaLog;
 import pt.up.fe.specs.clava.ClavaNode;
-import pt.up.fe.specs.clava.ast.omp.OmpDirectiveKind;
 import pt.up.fe.specs.clava.ast.omp.OmpPragma;
-import pt.up.fe.specs.clava.ast.omp.clauses.OmpClause;
 import pt.up.fe.specs.clava.ast.omp.clauses.OmpClauseKind;
-import pt.up.fe.specs.clava.ast.omp.clauses.OmpClauses;
-import pt.up.fe.specs.clava.ast.omp.clauses.OmpIntegerExpressionClause;
-import pt.up.fe.specs.clava.ast.omp.clauses.OmpListClause;
-import pt.up.fe.specs.clava.ast.omp.clauses.OmpProcBindClause;
 import pt.up.fe.specs.clava.ast.omp.clauses.OmpProcBindClause.ProcBindKind;
-import pt.up.fe.specs.clava.ast.omp.clauses.OmpReductionClause;
 import pt.up.fe.specs.clava.ast.omp.clauses.OmpReductionClause.ReductionKind;
 import pt.up.fe.specs.clava.weaver.abstracts.ACxxWeaverJoinPoint;
 import pt.up.fe.specs.clava.weaver.abstracts.joinpoints.AOmp;
@@ -65,188 +53,79 @@ public class CxxOmp extends AOmp {
 
     @Override
     public String getNumThreadsImpl() {
-        return OmpClauses.getNumThreads(ompPragma)
-                .map(OmpIntegerExpressionClause::getExpression)
-                .orElse(null);
-
-        /*
-        Optional<OmpClause> clause = ompPragma.getClause(NUM_THREADS);
-        
-        return clause
-                .map(OmpNumThreadsClause.class::cast)
-                .map(OmpNumThreadsClause::getExpression)
-                .orElse("");
-                */
+        return ompPragma.clauses().getNumThreads().orElse(null);
     }
 
     @Override
     public String getProcBindImpl() {
-
-        return OmpClauses.getProcBind(ompPragma)
-                .map(OmpProcBindClause::getprocBindKindString)
+        return ompPragma.clauses().getProcBind()
+                .map(ProcBindKind::getKey)
                 .orElse(null);
-        /*
-        Optional<OmpClause> clause = ompPragma.getClause(PROC_BIND);
-        
-        return clause
-                .map(OmpProcBindClause.class::cast)
-                .map(OmpProcBindClause::getprocBindKindString)
-                .orElse("");
-                */
     }
-
-    // @Override
-    // public String getNumThreadsImplOld() {
-    //
-    // OmpDirectiveKind directiveKind = ompPragma.getDirectiveKind();
-    //
-    // if (directiveKind.isClauseLegal(OmpClauseKind.NUM_THREADS)) {
-    //
-    // if (ompPragma instanceof OmpClausePragma) { // Clause directives
-    //
-    // OmpClausePragma clausePragma = (OmpClausePragma) ompPragma;
-    // Optional<OmpClause> clause = clausePragma.getClause(OmpClauseKind.NUM_THREADS);
-    //
-    // return clause
-    // .map(OmpNumThreadsClause.class::cast)
-    // .map(ompClause -> ompClause.getExpression())
-    // .orElse("");
-    // } else { // Special directives which are neither simple nor clause directives, e.g., #pragma omp atomic...
-    // throw new RuntimeException("Getting num_threads clause from a " + directiveKind + "("
-    // + ompPragma.getClass() + ") directive is not implmented yet: "
-    // + ompPragma.getLocation());
-    // }
-    // }
-    //
-    // // Simple directives or any other directives which do not support the clause
-    // ClavaLog.info("Cannot get num_threads clause from a " + directiveKind + " directive: "
-    // + ompPragma.getLocation());
-    // return "";
-    // }
-
-    // @Override
-    // public String getProcBindImplOld() {
-    //
-    // OmpDirectiveKind directiveKind = ompPragma.getDirectiveKind();
-    //
-    // if (directiveKind.isClauseLegal(OmpClauseKind.PROC_BIND)) {
-    //
-    // if (ompPragma instanceof OmpClausePragma) { // Clause directives
-    //
-    // OmpClausePragma clausePragma = (OmpClausePragma) ompPragma;
-    // Optional<OmpClause> clause = clausePragma.getClause(OmpClauseKind.PROC_BIND);
-    //
-    // return clause
-    // .map(OmpProcBindClause.class::cast)
-    // .map(ompClause -> ompClause.getKind().getString())
-    // .orElse("");
-    // } else { // Special directives which are neither simple nor clause directives, e.g., #pragma omp atomic...
-    // throw new RuntimeException("Getting proc_bind clause from a " + directiveKind + "("
-    // + ompPragma.getClass() + ") directive is not implmented yet: "
-    // + ompPragma.getLocation());
-    // }
-    // }
-    //
-    // // Simple directives or any other directives which do not support the clause
-    // ClavaLog.info("Cannot get proc_bind clause from a " + directiveKind + " directive: "
-    // + ompPragma.getLocation());
-    // return "";
-    // }
 
     @Override
     public Boolean hasClauseImpl(String clauseName) {
-        OmpClauseKind clauseKind = OmpClauseKind.getHelper().valueOf(clauseName);
+        OmpClauseKind clauseKind = parseClauseName(clauseName);
+        // if (clauseKind == null) {
+        // return false;
+        // }
+
         return ompPragma.hasClause(clauseKind);
+    }
+
+    private OmpClauseKind parseClauseName(String clauseName) {
+        return OmpClauseKind.getHelper().valueOf(clauseName);
+        // OmpClauseKind clauseKind = OmpClauseKind.getHelper().valueOfTry(clauseName).orElse(null);
+        // if (clauseKind == null) {
+        //
+        // }
+        // return clauseKind;
     }
 
     @Override
     public Boolean isClauseLegalImpl(String clauseName) {
-        OmpClauseKind clauseKind = OmpClauseKind.getHelper().valueOf(clauseName);
+        OmpClauseKind clauseKind = parseClauseName(clauseName);
+        // if (clauseKind == null) {
+        // return false;
+        // }
+
         return ompPragma.getDirectiveKind().isClauseLegal(clauseKind);
     }
 
     @Override
     public void setNumThreadsImpl(String newExpr) {
-        setClause(OmpClauses.newNumThreads(newExpr));
-        /*
-        OmpDirectiveKind directiveKind = ompPragma.getDirectiveKind();
-        
-        if (!directiveKind.isClauseLegal(NUM_THREADS)) {
-            ClavaLog.info("Can't set '" + NUM_THREADS.getString() + "' value on a " + directiveKind.getString()
-                    + " directive.");
-            return;
-        }
-        
-        OmpNumThreadsClause clause = new OmpNumThreadsClause(newExpr);
-        
-        ompPragma.addClause(NUM_THREADS, clause);
-        */
+        ompPragma.clauses().setNumThreads(newExpr);
     }
 
     @Override
     public void setProcBindImpl(String newBind) {
-        ProcBindKind kind = ProcBindKind.getHelper().valueOfTry(newBind).orElse(null);
-        if (kind == null) {
-            ClavaLog.info("Can't set '" + newBind + "' as a proc bind value, valid values: "
-                    + ProcBindKind.getHelper().getAvailableOptions());
-            return;
-        }
+        ProcBindKind kind = ProcBindKind.getHelper().valueOfTry(newBind)
+                .orElseThrow(() -> new RuntimeException("Can't set '" + newBind
+                        + "' as a proc bind value, valid values: " + ProcBindKind.getHelper().getAvailableOptions()));
+        ompPragma.clauses().setProcBind(kind);
+        // ProcBindKind kind = ProcBindKind.getHelper().valueOfTry(newBind).orElse(null);
+        // if (kind == null) {
+        // ClavaLog.info("Can't set '" + newBind + "' as a proc bind value, valid values: "
+        // + ProcBindKind.getHelper().getAvailableOptions());
+        // return;
+        // }
 
-        setClause(new OmpProcBindClause(kind));
+        // setClause(new OmpProcBindClause(kind));
 
-        /*        
-        OmpDirectiveKind directiveKind = ompPragma.getDirectiveKind();
-        
-        if (!directiveKind.isClauseLegal(PROC_BIND)) {
-            ClavaLog.info("Can't set '" + PROC_BIND.getString() + "' value on a " + directiveKind.getString()
-                    + " directive.");
-            return;
-        }
-        
-        ProcBindKind kind = ProcBindKind.getHelper().valueOf(newBind);
-        
-        OmpProcBindClause clause = new OmpProcBindClause(kind);
-        
-        ompPragma.addClause(PROC_BIND, clause);
-        */
     }
 
-    private void setClause(OmpClause clause) {
-        OmpDirectiveKind directiveKind = ompPragma.getDirectiveKind();
-
-        if (!directiveKind.isClauseLegal(clause.getKind())) {
-            ClavaLog.info("Can't set '" + clause.getKind().getString() + "' value on a " + directiveKind.getString()
-                    + " directive.");
-            return;
-        }
-
-        ompPragma.setClause(clause);
-    }
+    // private void setClause(OmpClause clause) {
+    // ompPragma.setClause(clause);
+    // }
 
     @Override
     public String[] getPrivateArrayImpl() {
-        // Optional<List<OmpListClause>> clauses = OmpClauses.getListClause(ompPragma, PRIVATE);
-        //
-        // if (!clauses.isPresent()) {
-        // return new String[0];
-        // }
-
-        return OmpClauses.getListClause(ompPragma, PRIVATE).stream()
-                .flatMap(clauseList -> clauseList.getVariables().stream())
-                .collect(Collectors.toList())
-                .toArray(new String[0]);
-
-        // return variables.toArray(new )
-        // .toArray(variableList -> new String[variableList.size()]);
-
-        // return OmpClauses.getProcBind(ompPragma)
-        // .map(OmpProcBindClause::getprocBindKindString)
-        // .orElse("");
+        return ompPragma.clauses().getPrivate().toArray(new String[0]);
     }
 
     @Override
     public void setPrivateImpl(String[] newVariables) {
-        ompPragma.setClause(new OmpListClause(PRIVATE, Arrays.asList(newVariables)));
+        ompPragma.clauses().setPrivate(Arrays.asList(newVariables));
     }
 
     @Override
@@ -259,38 +138,13 @@ public class CxxOmp extends AOmp {
 
     @Override
     public String[] reductionArrayImpl(String kind) {
-        String parsedKind = kind.toLowerCase();
-
-        return ompPragma.getClause(REDUCTION).stream()
-                .map(OmpReductionClause.class::cast)
-                .filter(reduction -> reduction.getReductionKind().getKey().equals(parsedKind))
-                .flatMap(reduction -> reduction.getVariables().stream())
-                .collect(Collectors.toList())
-                .toArray(new String[0]);
+        return ompPragma.clauses().getReduction(kind).toArray(new String[0]);
     }
 
     @Override
     public void setReductionImpl(String reductionKindString, String[] newVariables) {
-        System.out.println("Reduction kind:" + reductionKindString);
-        System.out.println("New variables:" + Arrays.toString(newVariables));
-
         ReductionKind reductionKind = ReductionKind.getHelper().valueOf(reductionKindString.toLowerCase());
 
-        // Get all reduction clauses
-        List<OmpClause> reductionClause = ompPragma.getClause(REDUCTION);
-
-        List<OmpClause> newReductionClause = new ArrayList<>();
-
-        // Store all reductions that are not of the kind to set
-        reductionClause.stream()
-                .map(OmpReductionClause.class::cast)
-                .filter(reduction -> reduction.getReductionKind() != reductionKind)
-                .forEach(newReductionClause::add);
-
-        // Add new reduction
-        newReductionClause.add(new OmpReductionClause(reductionKind, Arrays.asList(newVariables)));
-
-        ompPragma.setClause(newReductionClause);
-
+        ompPragma.clauses().setReductionImpl(reductionKind, Arrays.asList(newVariables));
     }
 }
