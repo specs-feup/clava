d692cc2c20c5b6b1381ce98eeb445caa53e41dc7
pedro <p.pinto@fe.up.pt>
2017-11-29 17:21:49 +0000

    [ClavaAst] Implements hashCode and equals for some type related classes

--- ClavaAst/src/pt/up/fe/specs/clava/ast/type/BuiltinType.java
+++ ClavaAst/src/pt/up/fe/specs/clava/ast/type/BuiltinType.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2016 SPeCS.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
@@ -126,6 +126,41 @@ public class BuiltinType extends Type {
         }
 
         return hasVoid;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((getTypeData() == null) ? 0 : getTypeData().hashCode());
+        result = prime * result + ((keywords == null) ? 0 : keywords.hashCode());
+
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        BuiltinType other = (BuiltinType) obj;
+
+        if (keywords == null) {
+            if (other.keywords != null) {
+                return false;
+            }
+        } else if (!keywords.equals(other.keywords)) {
+            return false;
+        }
+
+        return true;
     }
 
 }
--- ClavaAst/src/pt/up/fe/specs/clava/ast/type/data/TypeData.java
+++ ClavaAst/src/pt/up/fe/specs/clava/ast/type/data/TypeData.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2017 SPeCS.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
@@ -25,7 +25,7 @@ public class TypeData {
 
     /**
      * Helper method for cases when we just have the bare type string.
-     * 
+     *
      * @param bareType
      */
     public TypeData(String bareType) {
@@ -79,7 +79,7 @@ public class TypeData {
 
     /**
      * Number of expected children, just from the base TypeData.
-     * 
+     *
      * @return
      */
     public int getNumBaseChildren() {
@@ -90,6 +90,60 @@ public class TypeData {
         }
 
         return counter;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((bareType == null) ? 0 : bareType.hashCode());
+        result = prime * result + (containsUnexpandedParameterPack ? 1231 : 1237);
+        result = prime * result + (hasSugar ? 1231 : 1237);
+        result = prime * result + (isDependent ? 1231 : 1237);
+        result = prime * result + (isFromAst ? 1231 : 1237);
+        result = prime * result + (isVariablyModified ? 1231 : 1237);
+        result = prime * result + ((typeDependency == null) ? 0 : typeDependency.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        TypeData other = (TypeData) obj;
+        if (bareType == null) {
+            if (other.bareType != null) {
+                return false;
+            }
+        } else if (!bareType.equals(other.bareType)) {
+            return false;
+        }
+        if (containsUnexpandedParameterPack != other.containsUnexpandedParameterPack) {
+            return false;
+        }
+        if (hasSugar != other.hasSugar) {
+            return false;
+        }
+        if (isDependent != other.isDependent) {
+            return false;
+        }
+        if (isFromAst != other.isFromAst) {
+            return false;
+        }
+        if (isVariablyModified != other.isVariablyModified) {
+            return false;
+        }
+        if (typeDependency != other.typeDependency) {
+            return false;
+        }
+        return true;
     }
 
 }
