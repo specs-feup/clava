61a3d34ada9df4243805225db64fcc03df6828bb
JoaoBispo <joaobispo@gmail.com>
2017-12-19 22:32:27 +0000

    [ClavaWeaver] Adds API test 'UserValues' and weaver test 'SetType'

---
+++ ClavaWeaver/resources/clava/test/weaver/c/src/set_type.c
@@ -0,0 +1,11 @@
+const double constDouble = 0.0;
+
+int fooType(int a, double b);
+
+int fooType2(int a, double b);
+
+int fooType3(double a, double b);
+
+int fooType(int a, double b) {
+    return a + (int) b;
+}
