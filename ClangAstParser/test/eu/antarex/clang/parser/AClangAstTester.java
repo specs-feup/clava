4533f637b079adff6602507e64bd49f3ac73dd5e
JoaoBispo <joaobispo@gmail.com>
2018-02-06 02:28:53 +0000

    [ClangAstParser] Deletes commented code

--- ClangAstParser/test/eu/antarex/clang/parser/AClangAstTester.java
+++ ClangAstParser/test/eu/antarex/clang/parser/AClangAstTester.java
@@ -186,70 +186,11 @@ public abstract class AClangAstTester {
         clavaAst.write(new File(AClangAstTester.OUTPUT_FOLDERNAME),
                 SpecsIo.mkdir(AClangAstTester.OUTPUT_FOLDERNAME + "/outputFirst"));
 
-        /*
-        // Collect implementation files
-        Map<String, File> allFiles = SpecsIo.getFileMap(Arrays.asList(workFolder),
-                SourceType.getPermittedExceptions());
-        
-        List<String> implementationFiles = allFiles.keySet().stream()
-                .filter(SourceType.IMPLEMENTATION::hasExtension)
-                .collect(Collectors.toList());
-        
-        // Add C++ sources
-        // files.addAll(SpecsCollections.map(SpecsIo.getFiles(new File(AClangAstTester.OUTPUT_FOLDERNAME), "cpp"),
-        // file -> SpecsIo.getCanonicalPath(file)));
-        
-        // Add C sources
-        // files.addAll(SpecsCollections.map(SpecsIo.getFiles(new File(AClangAstTester.OUTPUT_FOLDERNAME), "c"),
-        // file -> SpecsIo.getCanonicalPath(file)));
-        
-        // Add OpenCL sources
-        // files.addAll(SpecsCollections.map(SpecsIo.getFiles(new File(AClangAstTester.OUTPUT_FOLDERNAME), "cl"),
-        // file -> SpecsIo.getCanonicalPath(file)));
-        
-        // Parse files
-        ClangRootNode ast = new ClangAstParser(showClangDump).parse(implementationFiles, compilerOptions);
-        
-        if (showClangDump) {
-            SpecsLogs.msgInfo("Clang Dump:\n" + SpecsIo.read(new File(ClangAstParser.getClangDumpFilename())));
-        }
-        
-        if (showClangAst) {
-            SpecsLogs.msgInfo("Clang AST:\n" + ast);
-        }
-        
-        // Parse dump information
-        try (ClavaParser clavaParser = new ClavaParser(ast)) {
-            App clavaAst = clavaParser.parse();
-            clavaAst.setSources(allFiles);
-        
-            if (showClavaAst) {
-                SpecsLogs.msgInfo("CLAVA AST:\n" + clavaAst);
-            }
-        
-            if (showCode) {
-                SpecsLogs.msgInfo("Code:\n" + clavaAst.getCode());
-            }
-            // System.out.println("AST:\n" + clavaAst);
-        
-            // Write
-            // clavaAst.writeFromTopFile(new File(OUTPUT_FOLDERNAME + "/" + mainFile),
-            // IoUtils.safeFolder(OUTPUT_FOLDERNAME + "/outputFirst"));
-        
-            clavaAst.write(new File(AClangAstTester.OUTPUT_FOLDERNAME),
-                    SpecsIo.mkdir(AClangAstTester.OUTPUT_FOLDERNAME + "/outputFirst"));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        */
         if (onePass) {
             return;
         }
 
         CodeParser testCodeParser = new CodeParser();
-
-        // Parse output again, check if files are the same
-        // List<String> files2 = new ArrayList<>();
 
         File firstOutputFolder = new File(AClangAstTester.OUTPUT_FOLDERNAME + "/outputFirst");
 
@@ -257,57 +198,6 @@ public abstract class AClangAstTester {
         testClavaAst.write(new File(AClangAstTester.OUTPUT_FOLDERNAME + "/outputFirst/"),
                 SpecsIo.mkdir(AClangAstTester.OUTPUT_FOLDERNAME + "/outputSecond"));
 
-        /*
-        // Collect implementation files
-        Map<String, File> allFiles2 = SpecsIo.getFileMap(Arrays.asList(firstOutputFolder),
-                SourceType.getPermittedExceptions());
-        
-        List<String> implementationFiles2 = allFiles2.keySet().stream()
-                .filter(SourceType.IMPLEMENTATION::hasExtension)
-                .collect(Collectors.toList());
-        
-        // Implementation files
-        // List<String> files2 = SpecsIo
-        // .getFiles(Arrays.asList(firstOutputFolder), SourceType.IMPLEMENTATION.getExtensions()).stream()
-        // .map(file -> SpecsIo.getCanonicalPath(file))
-        // .collect(Collectors.toList());
-        
-        // C++ sources
-        // files2.addAll(
-        // SpecsCollections.map(SpecsIo.getFiles(firstOutputFolder, "cpp"),
-        // file -> SpecsIo.getCanonicalPath(file)));
-        
-        // C sources
-        // files2.addAll(
-        // SpecsCollections.map(SpecsIo.getFiles(firstOutputFolder, "c"), file -> SpecsIo.getCanonicalPath(file)));
-        
-        // OpenCL sources
-        
-        // files2.addAll(
-        // SpecsCollections.map(SpecsIo.getFiles(firstOutputFolder, "cl"),
-        // file -> SpecsIo.getCanonicalPath(file)));
-        
-        // List<String> secondCompilerOptions = new ArrayList<>(compilerOptions);
-        
-        // Add first output folder to includes
-        // secondCompilerOptions.add("\"-I" + firstOutputFolder.getAbsolutePath() + "\"");
-        
-        // Parse files
-        ClangRootNode ast2 = new ClangAstParser().parse(implementationFiles2, compilerOptions);
-        
-        // Parse dump information
-        try (ClavaParser clavaParser = new ClavaParser(ast2)) {
-            App clavaAst2 = clavaParser.parse();
-            clavaAst2.setSources(allFiles2);
-            // Write
-            // clavaAst2.writeFromTopFile(new File(OUTPUT_FOLDERNAME + "/outputFirst/" + mainFile),
-            // IoUtils.safeFolder(OUTPUT_FOLDERNAME + "/outputSecond"));
-            clavaAst2.write(new File(AClangAstTester.OUTPUT_FOLDERNAME + "/outputFirst/"),
-                    SpecsIo.mkdir(AClangAstTester.OUTPUT_FOLDERNAME + "/outputSecond"));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        */
         // Test if files from first and second are the same
         Map<String, File> outputFiles1 = SpecsIo.getFiles(new File(AClangAstTester.OUTPUT_FOLDERNAME + "/outputFirst"))
                 .stream()
@@ -323,31 +213,7 @@ public abstract class AClangAstTester {
 
             Assert.assertNotNull("Could not find second version of file '" + name + "'", outputFile2);
 
-            // List<String> lines1 = StringLines.getLines(outputFiles1.get(name));
-            // List<String> lines2 = StringLines.getLines(outputFile2);
-
-            // Print lines if they are not the same size
-            //
-            // if (lines1.size() != lines2.size()) {
-            // LoggingUtils.msgInfo("Expected code:\n" + lines1.stream().collect(Collectors.joining("\n")));
-            // LoggingUtils.msgInfo("Actual code:\n" + lines2.stream().collect(Collectors.joining("\n")));
-            // LoggingUtils
-            // .msgInfo("Expected number of lines to be the same. Expected (" + lines1.size() + ") vs Actual ("
-            // + lines2.size() + ")");
-            // }
-
-            // System.out.println("LINES1:\n" + lines1);
-            // System.out.println("LINES2:\n" + lines2);
-
-            // Assert.assertEquals("Expected number of lines to be the same", lines1.size(), lines2.size());
-
-            // Compare each line
-            // for (int i = 0; i < lines1.size(); i++) {
-            // Assert.assertEquals("Expected lines to be the same", lines1.get(i), lines2.get(i));
-            // }
         }
-
-        // System.out.println("AST:\n" + ast2);
 
         // Compare with .txt, if available
         for (ResourceProvider resource : resources) {
@@ -363,12 +229,7 @@ public abstract class AClangAstTester {
             String txtContents = SpecsStrings.normalizeFileContents(SpecsIo.getResource(txtResource));
             File generatedFile = outputFiles2.get(resource.getFilename());
             String generatedFileContents = SpecsStrings.normalizeFileContents(SpecsIo.read(generatedFile));
-            // System.out.println(
-            // "CODE:" + IntStream.range(0, txtContents.length()).mapToObj(i -> (int) txtContents.charAt(i))
-            // .collect(Collectors.toList()));
-            // String file = IoUtils.read(generatedFile);
-            // System.out.println("FILE:" + IntStream.range(0, file.length()).mapToObj(i -> (int) file.charAt(i))
-            // .collect(Collectors.toList()));
+
             Assert.assertEquals(txtContents, generatedFileContents);
         }
     }
