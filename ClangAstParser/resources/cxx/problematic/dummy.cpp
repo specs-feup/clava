209a6fb51bd0d55cc9d14c5fe7c97e9bb437ece7
pedro <p.pinto@fe.up.pt>
2018-03-15 17:26:11 +0000

    [ClangAstParser] Adds constexpr dummy test

--- ClangAstParser/resources/cxx/problematic/dummy.cpp
+++ ClangAstParser/resources/cxx/problematic/dummy.cpp
@@ -1,31 +1,10 @@
-#include <vector>
+#include <array>
 
-class Box {
-  public:
-	Box(std::initializer_list<int>) {};
-  
+class A
+{
+ public:
+ static constexpr int number_of_sizes = 3;
+ static constexpr std::array<int, number_of_sizes> SPACE{{512, 256, 128}};
 };
 
-int main() {
-
-	std::vector<int> v = {};
-	Box b = {};
-
-/*
-	std::vector<int> t = std::vector<int> {};
-   	t = {};
-	std::vector<int> v = {};
-	Box b = {};
-
-	
-	
-	// allocates memory for a class
-   Box * box = new Box;
-   // allocates memory for a class, with nothrow
-   Box * box2 = new (std::nothrow) Box;
-   // allocates and constructs five objects (nothrow):
-   std::vector<int> * p2 = new (std::nothrow) std::vector<int>[5];
-	*/
-	return 0;
-}
-
+constexpr std::array<int, A::number_of_sizes> A::SPACE;
