f12201f2dcf5da67c4e48b24f13a718c639e0f33
pedro <p.pinto@fe.up.pt>
2018-03-15 18:25:35 +0000

    [ClangAstParser] Updates constexpr test

--- ClangAstParser/resources/cxx/problematic/dummy.cpp
+++ ClangAstParser/resources/cxx/problematic/dummy.cpp
@@ -1,10 +1,26 @@
 #include <array>
 
+
+class B {
+    
+};
+
 class A
 {
  public:
  static constexpr int number_of_sizes = 3;
  static constexpr std::array<int, number_of_sizes> SPACE{{512, 256, 128}};
+ static constexpr B SPACE2{};
+
+ int a;
+ 
+ void xpto();
 };
 
 constexpr std::array<int, A::number_of_sizes> A::SPACE;
+constexpr B A::SPACE2;
+
+void A::xpto() {
+    
+    return;
+}
