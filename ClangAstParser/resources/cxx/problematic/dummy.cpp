09b129f79ec43e91be01f1881cbd9d724e5c588d
JoaoBispo <joaobispo@gmail.com>
2018-03-17 01:46:21 +0000

    [ClangAstParser] Moves code from dummy test to C++ 'templates'

--- ClangAstParser/resources/cxx/problematic/dummy.cpp
+++ ClangAstParser/resources/cxx/problematic/dummy.cpp
@@ -1,62 +1,8 @@
-#include <array>
-#include <vector>
-#include <cstdlib>
-#include <algorithm>
 
-class B {
-    
-};
-
-class A
-{
- public:
- static constexpr int number_of_sizes = 3;
- static constexpr std::array<int, number_of_sizes> SPACE{{512, 256, 128}};
- static constexpr B SPACE2{};
-
- int a;
- 
- void xpto();
-};
-
-constexpr std::array<int, A::number_of_sizes> A::SPACE;
-constexpr B A::SPACE2;
-
-void A::xpto() {
-    
-    return;
-}
-
-
-template< typename T >
-void matrix_mult_tiling(const std::vector<T>& A , const std::vector<T>& B, std::vector<T>& C, const int N, const int M, const int K, const int BS1, const int BS2) {
-
-   for(int i=0; i<N; i++) {
-       for(int j=0; j<K; j++) {
-           C[K*i + j] = 0;
-       }
-    }
-
-    for(int l2=0; l2<M; l2 += BS1) {
-        for(int j2=0; j2<K; j2 += BS2) {
-            for(int i=0; i<N; i++) {
-                for(int l=l2; l< std::min(M, l2+BS1); l++) {
-                    for(int j=j2; j< std::min(K, j2+BS2); j++) {
-                        C[K*i + j] += A[M*i+l]*B[K*l+j];
-                    }
-                }
-            }
-        }
-    }
-}
 
 int main() {
     
-    // declare the matrices
-    std::vector<double> m1,m2,m3;
-
-    // execute the kernel
-    matrix_mult_tiling(m1, m2, m3, 2, 4, 7, 2, 2);
-    
+	// Nothing to test for now 
+	
     return 0;
 }
--- ClangAstParser/resources/cxx/templates.cpp
+++ ClangAstParser/resources/cxx/templates.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <list>
+#include <vector>
 
 
 struct MAtom {
@@ -18,7 +19,27 @@ inline t Distance2(const t a[3], const t b[3])
 				 (a[2] - b[2]) * (a[2] - b[2]);
 }
 
+template< typename T >
+void matrix_mult_tiling(const std::vector<T>& A , const std::vector<T>& B, std::vector<T>& C, const int N, const int M, const int K, const int BS1, const int BS2) {
 
+   for(int i=0; i<N; i++) {
+       for(int j=0; j<K; j++) {
+           C[K*i + j] = 0;
+       }
+    }
+
+    for(int l2=0; l2<M; l2 += BS1) {
+        for(int j2=0; j2<K; j2 += BS2) {
+            for(int i=0; i<N; i++) {
+                for(int l=l2; l< std::min(M, l2+BS1); l++) {
+                    for(int j=j2; j< std::min(K, j2+BS2); j++) {
+                        C[K*i + j] += A[M*i+l]*B[K*l+j];
+                    }
+                }
+            }
+        }
+    }
+}
 
 // TODO: TypeParser not supporting some nodes
 /*
@@ -46,6 +67,10 @@ template void f(int); // instantiates f<int>(int), template argument deduced
 */
 
 int main() {
-		
+    
+	// calling template function creates specialized versions that need to be removed
+    std::vector<double> m1,m2,m3;
+    matrix_mult_tiling(m1, m2, m3, 2, 4, 7, 2, 2);
+	
 	return 0;
 }
