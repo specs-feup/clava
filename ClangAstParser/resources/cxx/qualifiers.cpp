7939a8ddbf4d8ef02daeef469f7539e030358b0d
JoaoBispo <joaobispo@gmail.com>
2018-03-17 01:12:59 +0000

    [ClangAstParser] Updates C++ test 'qualifiers'

--- ClangAstParser/resources/cxx/qualifiers.cpp
+++ ClangAstParser/resources/cxx/qualifiers.cpp
@@ -1,3 +1,4 @@
+#include <array>
 
 const char *foo1(const double arg1[3]);
 const char mypass_version[] = "1.0";
@@ -7,10 +8,21 @@ class A
 {
  public:
  void constFoo() const;
+ static constexpr int number_of_sizes = 3;
+ static constexpr std::array<int, number_of_sizes> SPACE{{512, 256, 128}};
+ static constexpr std::array<int, number_of_sizes> SPACE2 = {512, 256, 128};
+ static constexpr std::array<std::array<int, 2>, 1> ARRAY_IN_ARRAY{{{{1, 2}}}};
+ static constexpr std::array<std::array<int, 2>, 1> ARRAY_IN_ARRAY2 = {{1,2}}; 
 };
 
 inline void A::constFoo() const {
 }
+
+// constexpr static members need to be allocated outside os the class/structure
+constexpr std::array<int, A::number_of_sizes> A::SPACE;
+constexpr std::array<int, A::number_of_sizes> A::SPACE2;
+constexpr std::array<std::array<int, 2>, 1> A::ARRAY_IN_ARRAY;
+constexpr std::array<std::array<int, 2>, 1> A::ARRAY_IN_ARRAY2;
 
 int main()
 {
@@ -23,7 +35,7 @@ int main()
         int n1;
         mutable int n2;
     } x = {0, 0};      // const object with mutable member
- 
+	
     n1 = 1; // ok, modifiable object
 //  n2 = 2; // error: non-modifiable object
     n4 = 3; // ok, treated as a side-effect
@@ -49,4 +61,9 @@ int main()
 	// constant pointer to a constant integer
 	int const * const constant4 = nullptr;
 	
+	const struct
+    {
+        int n1;
+    } x2 = {0};      // const object with single member
+
 }
