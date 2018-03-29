6200f3899bba3ae4680f849b5aaf94aee39fc7f1
JoaoBispo <joaobispo@gmail.com>
2018-03-29 02:48:46 +0100

    [ClangAstParser] Moves class CXXConversionDeclParser to right package

--- ClangAstParser/src/pt/up/fe/specs/clang/clavaparser/decl/CXXConversionDeclParser.java
+++ ClangAstParser/src/pt/up/fe/specs/clang/clavaparser/decl/CXXConversionDeclParser.java
@@ -11,7 +11,7 @@
  * specific language governing permissions and limitations under the License. under the License.
  */
 
-package pt.up.fe.specs.clang.clavaparser.expr;
+package pt.up.fe.specs.clang.clavaparser.decl;
 
 import pt.up.fe.specs.clang.ast.ClangNode;
 import pt.up.fe.specs.clang.clavaparser.AClangNodeParser;
