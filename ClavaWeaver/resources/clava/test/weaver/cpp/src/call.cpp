18227233a20d0dc46f3debcfb91d3016b51cabab
JoaoBispo <joaobispo@gmail.com>
2018-04-18 00:16:42 +0100

    [ClavaWeaver] Changes test 'call'

--- ClavaWeaver/resources/clava/test/weaver/cpp/src/call.cpp
+++ ClavaWeaver/resources/clava/test/weaver/cpp/src/call.cpp
@@ -2,7 +2,21 @@ int foo(int a, int b) {
 	return a + b;
 }
 
+class A {
+	public:
+	int foo2();
+};
+
+class B {
+	
+	public:
+	A a;
+};
+
 int main() {
 	int x = 2 + 3;
 	foo(1, 2);
+
+	B b;
+	b.a.foo2();
 }
