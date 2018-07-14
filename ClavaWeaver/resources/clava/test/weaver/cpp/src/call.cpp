2a7b685b8d8dac997d09497c0f59c09b7aa9d98e
JoaoBispo <joaobispo@gmail.com>
2018-07-14 19:11:56 +0100

    [ClavaWeaver] Improves C++ test 'Call'

--- ClavaWeaver/resources/clava/test/weaver/cpp/src/call.cpp
+++ ClavaWeaver/resources/clava/test/weaver/cpp/src/call.cpp
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int foo(int a, int b) {
 	return a + b;
 }
@@ -17,10 +19,35 @@ class B {
 	A a;
 };
 
+void functionDecl();
+
+void functionDef() {
+	printf("functionDef\n");
+}
+
+void functionDeclAndDef();
+
+void functionDeclAndDef() {
+	printf("functionDeclAndDef\n");
+}
+
+void functionDefAndDecl() {
+	printf("functionDefAndDecl\n");
+}
+
+void functionDefAndDecl();
+
+
 int main() {
 	int x = 2 + 3;
 	foo(1, 2);
 
 	B b;
 	b.a.foo2();
+	
+	functionDecl();
+	functionDef();
+	functionDeclAndDef();
+	functionDefAndDecl();
+	printf(".function test");
 }
