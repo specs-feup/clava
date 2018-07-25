69133f044e9d879154936b8265afa93fe94bdc47
JoaoBispo <joaobispo@gmail.com>
2018-07-26 00:01:51 +0100

    [ClangAstParser] Updates ClangAstLauncher to use CodeParser

--- ClangAstParser/src/pt/up/fe/specs/clang/ClangAstLauncher.java
+++ ClangAstParser/src/pt/up/fe/specs/clang/ClangAstLauncher.java
@@ -22,12 +22,8 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
-import org.suikasoft.jOptions.Interfaces.DataStore;
-
 import pt.up.fe.specs.clang.ast.JsonUtils;
-import pt.up.fe.specs.clang.ast.genericnode.ClangRootNode;
-import pt.up.fe.specs.clang.clavaparser.ClavaParser;
-import pt.up.fe.specs.clava.ClavaOptions;
+import pt.up.fe.specs.clang.codeparser.CodeParser;
 import pt.up.fe.specs.clava.ast.extra.App;
 import pt.up.fe.specs.clava.ast.type.Type;
 import pt.up.fe.specs.util.SpecsIo;
@@ -43,7 +39,8 @@ public class ClangAstLauncher {
     private static final HashSet<String> C_EXTENSIONS = new HashSet<>(Arrays.asList(
             "c", "cpp", "h", "hpp", "hxx"));
 
-    private static final String CLANGAST_BINARY_VERSION = "v2.9";
+    // private static final String CLANGAST_BINARY_VERSION = "v2.9";
+    // private static final String CLANGAST_BINARY_VERSION = "v3.0.17";
 
     public static void main(String[] args) {
         SpecsProperty.ShowStackTrace.applyProperty("false");
@@ -55,7 +52,8 @@ public class ClangAstLauncher {
             return;
         }
 
-        List<String> files = new ArrayList<>();
+        // List<String> files = new ArrayList<>();
+        List<File> files = new ArrayList<>();
 
         // Add arguments while they are identified as C/C+ files
         int currentIndex = 0;
@@ -63,7 +61,7 @@ public class ClangAstLauncher {
             String currentArg = args[currentIndex];
             if (isValidFile(currentArg)) {
                 // arguments.add(currentArg);
-                files.add(currentArg);
+                files.add(new File(currentArg));
                 currentIndex++;
             } else {
                 break;
@@ -72,44 +70,65 @@ public class ClangAstLauncher {
 
         // If there still are arguments left using, pass them after '--'
         List<String> options = Arrays.asList(args).subList(currentIndex, args.length);
-        DataStore config = ClavaOptions.toDataStore(options);
+        // DataStore config = ClavaOptions.toDataStore(options);
 
         // Set version of ClangAst
-        config.set(ClangAstKeys.CLANGAST_VERSION, CLANGAST_BINARY_VERSION);
+        // config.set(ClangAstKeys.CLANGAST_VERSION, CLANGAST_BINARY_VERSION);
 
         // boolean useCustomResources = config.get(ClavaOptions.CUSTOM_RESOURCES);
         // ClangRootNode clangAst = new ClangAstParser(false, useCustomResources).parse(files, config);
 
-        ClangRootNode clangAst = new ClangAstParser().parse(files, config);
+        App clavaAst = CodeParser.newInstance().parse(files, options);
 
+        // Write AST
+        SpecsIo.write(new File("ast_parsed.txt"), clavaAst.toString());
+
+        // Write JSON for the AST
+        SpecsIo.write(new File("ast_parsed.json"), JsonUtils.toJson(clavaAst));
+
+        // Write nodes->type map
+        // Map<String, String> nodesToTypes = parseNodesToTypes(IoUtils.read("nodetypes.txt"));
+        SpecsIo.write(new File("nodes_to_types.json"), toJsonNodeToTypes(SpecsIo.read("nodetypes.txt")));
+
+        // Delete temp files, unless there is a 'debug' file
+        if (!new File("debug").isFile()) {
+            // Delete ClangAst files
+            ClangAstParser.getTempFiles().stream()
+                    .forEach(filename -> new File(filename).delete());
+        }
+
+        /*
+        
+        ClangRootNode clangAst = new ClangAstParser().parse(files, config);
+        
         // Parse dump information
         try (ClavaParser clavaParser = new ClavaParser(clangAst)) {
-
+        
             App clavaAst = clavaParser.parse();
-
+        
             // Write Clang output
             // IoUtils.write(new File("clang_output.txt"), clangAst.getClangOutput());
-
+        
             // Write AST
             SpecsIo.write(new File("ast_parsed.txt"), clavaAst.toString());
-
+        
             // Write JSON for the AST
             SpecsIo.write(new File("ast_parsed.json"), JsonUtils.toJson(clavaAst));
-
+        
             // Write JSON for the types
             SpecsIo.write(new File("types.json"), toJson(clavaParser.getTypes()));
-
+        
             // Write nodes->type map
             // Map<String, String> nodesToTypes = parseNodesToTypes(IoUtils.read("nodetypes.txt"));
             SpecsIo.write(new File("nodes_to_types.json"), toJsonNodeToTypes(SpecsIo.read("nodetypes.txt")));
-
+        
             // Delete temp files, unless there is a 'debug' file
             if (!new File("debug").isFile()) {
                 // Delete ClangAst files
                 ClangAstParser.getTempFiles().stream()
                         .forEach(filename -> new File(filename).delete());
             }
-
+        
         } catch (Exception e) {
             // If there is an exception and there is no output error, just show the output and exit
             String message = e.getMessage();
@@ -117,11 +136,11 @@ public class ClangAstLauncher {
             if (message.startsWith("USAGE")) {
                 System.exit(0);
             }
-
+        
             SpecsLogs.msgInfo("\n\nParsing aborted");
             System.exit(1);
         }
-
+        */
     }
 
     // private static Map<String, String> parseNodesToTypes(String string) {
