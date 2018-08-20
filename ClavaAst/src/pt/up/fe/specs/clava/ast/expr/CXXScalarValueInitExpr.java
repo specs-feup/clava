6d50917cfdf98a142ed025e71dd4a62efc5d8f26
JoaoBispo <joaobispo@gmail.com>
2018-08-20 19:02:34 +0100

    [ClavaAst] Adds CXXDynamicCastExpr and CXXScalarValueInitExpr

---
+++ ClavaAst/src/pt/up/fe/specs/clava/ast/expr/CXXScalarValueInitExpr.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2018 SPeCS.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package pt.up.fe.specs.clava.ast.expr;
+
+import java.util.Collection;
+
+import org.suikasoft.jOptions.Interfaces.DataStore;
+
+import pt.up.fe.specs.clava.ClavaNode;
+
+/**
+ * An expression "T()" which creates a value-initialized rvalue of type T, which is a non-class type.
+ * 
+ * See (C++98 [5.2.3p2]).
+ * 
+ * @author JoaoBispo
+ *
+ */
+public class CXXScalarValueInitExpr extends Expr {
+
+    public CXXScalarValueInitExpr(DataStore data, Collection<? extends ClavaNode> children) {
+        super(data, children);
+    }
+
+}
