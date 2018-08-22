53e5442c8c915cfdbe11733ad9fcfe380eee3780
JoaoBispo <joaobispo@gmail.com>
2018-08-22 16:16:41 +0100

    [ClangAstDumper] Disambiguates call to clava::dump()

--- ClangAstDumper/ClavaDataDumperTypes.cpp
+++ ClangAstDumper/ClavaDataDumperTypes.cpp
@@ -263,7 +263,7 @@ void clava::ClavaDataDumper::DumpBuiltinTypeData(const BuiltinType *T) {
     DumpTypeData(T);
 
     clava::dump(clava::BUILTIN_KIND[T->getKind()]);
-    clava::dump(T->getName(Context->getPrintingPolicy()));
+    clava::dump((const std::string&) T->getName(Context->getPrintingPolicy()));
 
 }
 
