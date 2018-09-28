3c0d43d109d46882e786e28bc4746640e4a50460
JoaoBispo <joaobispo@gmail.com>
2018-09-28 01:27:31 +0100

    [ClavaWeaver] Updates C++ test MultiFile

--- ClavaWeaver/resources/clava/test/weaver/cpp/src/multiFile.cpp
+++ ClavaWeaver/resources/clava/test/weaver/cpp/src/multiFile.cpp
@@ -5,4 +5,15 @@ int main() {
 
 	A a;
 
+	std::vector<double> vector_double;
+    std::vector<int> vector_int;
+    
+    template_foo(vector_double);
+    template_foo(vector_int);
+	
+	template_foo_2(vector_double);
+    template_foo_2(vector_int);
+	
+	template_foo_3(vector_double);
+    template_foo_3(vector_int);
 }
--- ClavaWeaver/resources/clava/test/weaver/cpp/src/multiFile.h
+++ ClavaWeaver/resources/clava/test/weaver/cpp/src/multiFile.h
@@ -1,4 +1,31 @@
+#include <vector>
+
 struct A {
 	int x;
 	int y;
 };
+
+template< typename T >
+void template_foo(const std::vector<T>& A);
+
+template< typename T >
+void template_foo(const std::vector<T>& A) {
+    
+}
+
+template< typename T >
+void template_foo_2(const std::vector<T>& A);
+
+template< typename T >
+void template_foo_2(const std::vector<T>& A) {
+    
+}
+
+
+template< typename T >
+void template_foo_3(const std::vector<T>& A);
+
+template< typename T >
+void template_foo_3(const std::vector<T>& A) {
+    
+}
