2e2dafbf1e275724e1b5a1d530ea4fc19f2ddf12
JoaoBispo <joaobispo@gmail.com>
2018-10-04 11:58:10 +0100

    [ClangAstParser] Updates C test 'macro'

--- ClangAstParser/resources/c/macro.c
+++ ClangAstParser/resources/c/macro.c
@@ -8,6 +8,9 @@ int main() {
 	// Macro that uses compound literal expression
 	double d1, d2;
 	double d3 = double_add(d1, d2);
+
+	// Macro in size_of
+	int size = custom_size(data_type);
 	
 	return 0;
 }
--- ClangAstParser/resources/c/macro.h
+++ ClangAstParser/resources/c/macro.h
@@ -1,3 +1,9 @@
 #define declInt(x,y)    int x = y;
 
 #define double_add(a,b)   (double){(a)+(b)}
+
+#define data_type double
+
+#define custom_size(type) sizeof(type)
+//#define type_decl(var, type)	\
+//  type var;
