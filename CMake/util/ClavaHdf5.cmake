db8ca36a46317573a7a7b3929d8616c6d4fe4f11
JoaoBispo <joaobispo@gmail.com>
2018-10-17 21:14:40 +0100

    [CMake] Adds hdf5-cpp library to generated target

--- CMake/util/ClavaHdf5.cmake
+++ CMake/util/ClavaHdf5.cmake
@@ -33,7 +33,9 @@ function(clava_generate_hdf5 ORIG_TARGET GENERATED_TARGET)
 	# Generate HDF5 interfaces for current code
 	clava_generate(${ORIG_TARGET} ${GENERATED_TARGET} ${HDF5_ASPECT} ${HDF5_ASPECT_ARGS} ${DEPENDENCIES}) # If attribute is prefixed by 'rx_', it is interpreted as a regex 
 	target_include_directories(${GENERATED_TARGET} PUBLIC ${HDF5_INCLUDE_DIRS})
-	target_link_libraries(${GENERATED_TARGET} ${HDF5_LIBRARIES} ${HDF5_CXX_LIBRARIES})
+	#target_link_libraries(${GENERATED_TARGET} ${HDF5_LIBRARIES} ${HDF5_CXX_LIBRARIES})
+	target_link_libraries(${GENERATED_TARGET} hdf5::hdf5-shared hdf5_cpp)
+	
 	
 	#"filter: {rx_name: 'A', kind: 'class'}") # If attribute is prefixed by 'rx_', it is interpreted as a regex
 	
