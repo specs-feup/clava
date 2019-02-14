667d6068766e74aca109ae3f6b54b9265a18f17b
JoaoBispo <joaobispo@gmail.com>
2019-02-14 18:56:20 +0000

    [ClavaWeaver] Changes C API test "Autopar"

--- ClavaWeaver/resources/clava/test/api/c/src/autopar_test.c
+++ ClavaWeaver/resources/clava/test/api/c/src/autopar_test.c
@@ -1,4 +1,4 @@
-double bar(int a);
+#include <stdio.h>
 
 double bar(int a) {
     return a + 10;
@@ -7,10 +7,25 @@ double bar(int a) {
 double foo() {
     double a = 0;
 
+    int numIter = 1000;
+    for(int i=0; i<numIter; i++) {
+        a+= i;
+    }
+
     for(int i=0; i<1000; i++) {
-        //a += i;
         a += bar(i);
     }
+    
+    for(int i=0; i<1000; i++) {
+        printf("Function with possible side-effects\n");
+    }
+    
+    #pragma clava data safeFunctions:['printf']
+    for(int i=0; i<1000; i++) {
+        printf("Ignore this function, consider it safe");
+    }
+    
+
     
     return a;
 }
