cb475972bf552880bc9a7c56842ddb40b332df72
JoaoBispo <joaobispo@gmail.com>
2019-03-28 21:16:00 +0000

    [ClangAstParser] Updates C test 'macro'

--- ClangAstParser/resources/c/macro.h
+++ ClangAstParser/resources/c/macro.h
@@ -7,3 +7,13 @@
 #define custom_size(type) sizeof(type)
 //#define type_decl(var, type)	\
 //  type var;
+
+#define EXTERN_DECL(type, array)  extern type array[]
+
+// These declarations will all have the same source location, 
+// comments will not be aligned with declarations
+EXTERN_DECL(int, extern_a); /* comment after extern_a */
+EXTERN_DECL(int, extern_b); // comment after extern_b
+EXTERN_DECL(int, extern_c);          
+EXTERN_DECL(int, extern_d);   
+
