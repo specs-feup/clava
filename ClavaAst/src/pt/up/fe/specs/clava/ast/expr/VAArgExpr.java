22cee284afd5b57ffa0543af003e5382c7093930
JoaoBispo <joaobispo@gmail.com>
2019-04-01 22:46:01 +0100

    [ClavaAst] Adds node VAArgExpr

---
+++ ClavaAst/src/pt/up/fe/specs/clava/ast/expr/VAArgExpr.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2019 SPeCS.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package pt.up.fe.specs.clava.ast.expr;
+
+import java.util.Collection;
+
+import org.suikasoft.jOptions.Interfaces.DataStore;
+
+import pt.up.fe.specs.clava.ClavaNode;
+
+public class VAArgExpr extends Expr {
+
+    public VAArgExpr(DataStore data, Collection<? extends ClavaNode> children) {
+        super(data, children);
+    }
+
+    public Expr getSubExpr() {
+        return getChild(Expr.class, 0);
+    }
+
+    @Override
+    public String getCode() {
+        StringBuilder code = new StringBuilder();
+
+        Expr subExpr = getSubExpr();
+
+        code.append("va_arg(");
+        code.append(subExpr.getCode());
+        code.append(", ");
+        code.append(getTypeCode());
+        code.append(")");
+
+        return code.toString();
+    }
+}
