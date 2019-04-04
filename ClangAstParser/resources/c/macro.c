f8cd7b3f7b6d5d0efc60671dcaaf19c347532234
JoaoBispo <joaobispo@gmail.com>
2019-04-04 18:07:00 +0100

    [ClangAstParser] Updates C test 'macro'

--- ClangAstParser/resources/c/macro.c
+++ ClangAstParser/resources/c/macro.c
@@ -1,5 +1,14 @@
 #include "macro.h"
 
+#define FOR_BB_BETWEEN(BB, FROM, TO, DIR) \
+  for (BB = FROM; BB != TO; BB = BB + 1)
+
+#define FOR_EACH_BB(BB) \
+  FOR_BB_BETWEEN (BB, ENTRY, EXIT, next_bb)
+
+int ENTRY = 0;  
+int EXIT = 10;
+  
 // Macro that instantiates a declaration
 declInt(a,0)
 
@@ -11,6 +20,16 @@ int main() {
 
 	// Macro in size_of
 	int size = custom_size(data_type);
+
+	int i;
+	
+	/**
+	 * A comment
+	 **/
+	FOR_EACH_BB (i)
+    {
+		;
+	}
 	
 	return 0;
 }
