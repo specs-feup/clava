6953621d3dcd1e1f72e2a15538215dbca164e29d
JoaoBispo <joaobispo@gmail.com>
2020-01-29 18:49:12 +0000

    [ClavaLaraApi] Adds 'round' and 'fmod' to list of AutoPar safe functions

--- ClavaLaraApi/src-js/clava/js/autopar/SafeFunctionCalls.js
+++ ClavaLaraApi/src-js/clava/js/autopar/SafeFunctionCalls.js
@@ -1,2 +1,2 @@
-var safefunctionCallslist = ['sqrt', 'log', 'fabs', 'malloc', 'pow', 'cos', 'sin', 'exp', 'floor', 'ceil'];
+var safefunctionCallslist = ['sqrt', 'log', 'fabs', 'malloc', 'pow', 'cos', 'sin', 'exp', 'floor', 'ceil', 'round', 'fmod'];
 
