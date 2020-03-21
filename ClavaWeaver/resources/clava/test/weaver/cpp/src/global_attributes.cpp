27c94133ea4c7dfec82af738283a63d5e6ee95cc
João Bispo <joaobispo@gmail.com>
2020-03-21 01:18:17 +0000

    [ClavaWeaver] Improves C++ weaver test 'GlobalAttributes'

--- ClavaWeaver/resources/clava/test/weaver/cpp/src/global_attributes.cpp
+++ ClavaWeaver/resources/clava/test/weaver/cpp/src/global_attributes.cpp
@@ -1,3 +1,19 @@
+void insideHeader() {
+	int a = 0;
+	
+	if(a > 0) {
+		if(a < 0) {
+		
+		}
+	}
+	
+	for(int i=0; i<10; i++) {
+		for(int j=0; j<10; j++) {
+		}
+	}
+
+}
+
 int main() {
 	
 	int a = 10;
