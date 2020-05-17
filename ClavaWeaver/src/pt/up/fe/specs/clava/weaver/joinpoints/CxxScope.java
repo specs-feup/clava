faccb7acbab3a83b0bda308a291ee11ff910bb9f
Tiago Lascasas dos Santos <tiagolascasas@hotmail.com>
2020-05-17 18:23:39 +0100

    [ClavaWeaver] Updates CFG action to use the new dot building call

--- ClavaWeaver/src/pt/up/fe/specs/clava/weaver/joinpoints/CxxScope.java
+++ ClavaWeaver/src/pt/up/fe/specs/clava/weaver/joinpoints/CxxScope.java
@@ -412,7 +412,7 @@ public class CxxScope extends AScope {
     @Override
     public void cfgImpl() {
 	ControlFlowGraph cfg = new ControlFlowGraph(scope);
-	ClavaLog.info(cfg.buildDot());
+	ClavaLog.info(cfg.toDot());
     }
 
     @Override
