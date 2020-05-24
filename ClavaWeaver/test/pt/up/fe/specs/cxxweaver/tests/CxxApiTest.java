dbb13f6de7aa8bec920129a7c690e719126382aa
João Bispo <joaobispo@gmail.com>
2020-05-24 14:13:47 +0100

    [ClavaWeaver] Adds C++ API test 'clava'

--- ClavaWeaver/test/pt/up/fe/specs/cxxweaver/tests/CxxApiTest.java
+++ ClavaWeaver/test/pt/up/fe/specs/cxxweaver/tests/CxxApiTest.java
@@ -128,4 +128,9 @@ public class CxxApiTest {
     public void testAddHeaderFile() {
         newTester().test("AddHeaderFileTest.lara", "add_header_file.h");
     }
+
+    @Test
+    public void testClava() {
+        newTester().test("ClavaTest.lara", "clava.cpp");
+    }
 }
