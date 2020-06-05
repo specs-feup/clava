bf403471c4d1c5624d1b9b7f5d9dc5438512c9e6
Tiago Lascasas dos Santos <tiagolascasas@hotmail.com>
2020-06-05 02:17:22 +0100

    [ClavaHls] Adds modes of operation to ClavaHls

---
+++ ClavaHls/src/pt/up/fe/specs/clava/hls/TracingValidator.java
@@ -0,0 +1,32 @@
+/**
+ *  Copyright 2020 SPeCS.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package pt.up.fe.specs.clava.hls;
+
+import pt.up.fe.specs.clava.analysis.flow.data.DataFlowGraph;
+
+public class TracingValidator {
+    private DataFlowGraph dfg;
+
+    public TracingValidator(DataFlowGraph dfg) {
+	this.dfg = dfg;
+    }
+
+    public boolean validate() {
+	return false;
+    }
+}
