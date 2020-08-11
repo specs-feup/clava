f0b3376ee5053b2151a76d4be51e3c7dbdee6fdc
João Bispo <joaobispo@gmail.com>
2020-08-11 19:21:34 +0100

    [TranslationUnitPatcher] ErrorPatcher now uses a Map instead of a switch

--- TranslationUnitPatcher/src/pt/up/fe/specs/tupatcher/ErrorPatcher.java
+++ TranslationUnitPatcher/src/pt/up/fe/specs/tupatcher/ErrorPatcher.java
@@ -14,12 +14,21 @@
 package pt.up.fe.specs.tupatcher;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
 
 import pt.up.fe.specs.tupatcher.parser.TUErrorData;
 import pt.up.fe.specs.tupatcher.parser.TUErrorsData;
+import pt.up.fe.specs.util.SpecsLogs;
 
 public class ErrorPatcher {
 
+    private final static Map<ErrorKind, BiConsumer<TUErrorData, PatchData>> ERROR_PATCHERS;
+    static {
+        ERROR_PATCHERS = new HashMap<>();
+
+        ERROR_PATCHERS.put(ErrorKind.UNKNOWN_TYPE, ErrorPatcher::unknownType);
+    }
     // final public static int UNKNOWN_TYPE = 3822;
 
     private final PatchData patchData;
@@ -28,28 +37,58 @@ public class ErrorPatcher {
         this.patchData = patchData;
     }
 
-    public void patch(TUErrorsData errorsData) {
-        for (TUErrorData data : errorsData.get(TUErrorsData.ERRORS)) {
-            int errorNumber = (int) data.getValue("errorNumber");
-
-            var error = ErrorKind.getKind(errorNumber);
-            switch (error) {
-            case UNKNOWN_TYPE:
-                // unknown type identifier
-                String typeName = ((HashMap<String, String>) (data.getValue("map"))).get("identifier_name");
-                patchData.addType(typeName);
-                break;
-            }
-            // if (errorNumber == ErrorKind.UNKNOWN_TYPE.getId()) {
-            // // unknown type identifier
-            // String typeName = ((HashMap<String, String>) (data.getValue("map"))).get("identifier_name");
-            // PatchData.addType(typeName);
-            //
-            // }
+    /**
+     * 
+     * @param errorsData
+     * @return true if patchData was updated, false otherwise
+     */
+    public boolean patch(TUErrorsData errorsData) {
+        var errors = errorsData.get(TUErrorsData.ERRORS);
+        if (errors.isEmpty()) {
+            SpecsLogs.info("No errors found!");
+            return false;
         }
+
+        // Try to correct only the first error
+        var data = errors.get(0);
+
+        // for (TUErrorData data : errorsData.get(TUErrorsData.ERRORS)) {
+        int errorNumber = (int) data.getValue("errorNumber");
+
+        var error = ErrorKind.getKind(errorNumber);
+
+        var errorPatcher = ERROR_PATCHERS.get(error);
+        if (errorPatcher == null) {
+            throw new RuntimeException("Error kind not supported yet: " + error);
+        }
+
+        errorPatcher.accept(data, patchData);
+        return true;
+        /*
+        switch (error) {
+        case UNKNOWN_TYPE:
+            // unknown type identifier
+            String typeName = ((HashMap<String, String>) (data.getValue("map"))).get("identifier_name");
+            patchData.addType(typeName);
+            break;
+        }
+        // if (errorNumber == ErrorKind.UNKNOWN_TYPE.getId()) {
+        // // unknown type identifier
+        // String typeName = ((HashMap<String, String>) (data.getValue("map"))).get("identifier_name");
+        // PatchData.addType(typeName);
+        //
+        // }
+                */
+        // }
         // else if (...) other errors
 
-        return;
+        // return;
+
+    }
+
+    public static void unknownType(TUErrorData data, PatchData patchData) {
+        String typeName = data.get(TUErrorData.MAP).get("identifier_name");
+        patchData.addType(typeName);
     }
 
 }
