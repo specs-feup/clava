3520fd2d19920b1636b8595a5b841cc01d0ae0d3
João Bispo <joaobispo@gmail.com>
2020-09-10 17:55:25 +0100

    [ClavaWeaver] Adds join points "switch" and "case"

---
+++ ClavaWeaver/src/pt/up/fe/specs/clava/weaver/abstracts/joinpoints/ACase.java
@@ -0,0 +1,593 @@
+package pt.up.fe.specs.clava.weaver.abstracts.joinpoints;
+
+import java.util.List;
+import java.util.Map;
+import org.lara.interpreter.weaver.interf.JoinPoint;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.Arrays;
+
+/**
+ * Auto-Generated class for join point ACase
+ * This class is overwritten by the Weaver Generator.
+ * 
+ * 
+ * @author Lara Weaver Generator
+ */
+public abstract class ACase extends AStatement {
+
+    protected AStatement aStatement;
+
+    /**
+     * 
+     */
+    public ACase(AStatement aStatement){
+        this.aStatement = aStatement;
+    }
+    /**
+     * Get value on attribute isFirst
+     * @return the attribute's value
+     */
+    @Override
+    public Boolean getIsFirstImpl() {
+        return this.aStatement.getIsFirstImpl();
+    }
+
+    /**
+     * Get value on attribute isLast
+     * @return the attribute's value
+     */
+    @Override
+    public Boolean getIsLastImpl() {
+        return this.aStatement.getIsLastImpl();
+    }
+
+    /**
+     * Method used by the lara interpreter to select exprs
+     * @return 
+     */
+    @Override
+    public List<? extends AExpression> selectExpr() {
+        return this.aStatement.selectExpr();
+    }
+
+    /**
+     * Method used by the lara interpreter to select childExprs
+     * @return 
+     */
+    @Override
+    public List<? extends AExpression> selectChildExpr() {
+        return this.aStatement.selectChildExpr();
+    }
+
+    /**
+     * Method used by the lara interpreter to select calls
+     * @return 
+     */
+    @Override
+    public List<? extends ACall> selectCall() {
+        return this.aStatement.selectCall();
+    }
+
+    /**
+     * Method used by the lara interpreter to select stmtCalls
+     * @return 
+     */
+    @Override
+    public List<? extends ACall> selectStmtCall() {
+        return this.aStatement.selectStmtCall();
+    }
+
+    /**
+     * Method used by the lara interpreter to select memberCalls
+     * @return 
+     */
+    @Override
+    public List<? extends AMemberCall> selectMemberCall() {
+        return this.aStatement.selectMemberCall();
+    }
+
+    /**
+     * Method used by the lara interpreter to select memberAccesss
+     * @return 
+     */
+    @Override
+    public List<? extends AMemberAccess> selectMemberAccess() {
+        return this.aStatement.selectMemberAccess();
+    }
+
+    /**
+     * Method used by the lara interpreter to select arrayAccesss
+     * @return 
+     */
+    @Override
+    public List<? extends AArrayAccess> selectArrayAccess() {
+        return this.aStatement.selectArrayAccess();
+    }
+
+    /**
+     * Method used by the lara interpreter to select vardecls
+     * @return 
+     */
+    @Override
+    public List<? extends AVardecl> selectVardecl() {
+        return this.aStatement.selectVardecl();
+    }
+
+    /**
+     * Method used by the lara interpreter to select varrefs
+     * @return 
+     */
+    @Override
+    public List<? extends AVarref> selectVarref() {
+        return this.aStatement.selectVarref();
+    }
+
+    /**
+     * Method used by the lara interpreter to select ops
+     * @return 
+     */
+    @Override
+    public List<? extends AOp> selectOp() {
+        return this.aStatement.selectOp();
+    }
+
+    /**
+     * Method used by the lara interpreter to select binaryOps
+     * @return 
+     */
+    @Override
+    public List<? extends ABinaryOp> selectBinaryOp() {
+        return this.aStatement.selectBinaryOp();
+    }
+
+    /**
+     * Method used by the lara interpreter to select unaryOps
+     * @return 
+     */
+    @Override
+    public List<? extends AUnaryOp> selectUnaryOp() {
+        return this.aStatement.selectUnaryOp();
+    }
+
+    /**
+     * Method used by the lara interpreter to select newExprs
+     * @return 
+     */
+    @Override
+    public List<? extends ANewExpr> selectNewExpr() {
+        return this.aStatement.selectNewExpr();
+    }
+
+    /**
+     * Method used by the lara interpreter to select deleteExprs
+     * @return 
+     */
+    @Override
+    public List<? extends ADeleteExpr> selectDeleteExpr() {
+        return this.aStatement.selectDeleteExpr();
+    }
+
+    /**
+     * Method used by the lara interpreter to select cilkSpawns
+     * @return 
+     */
+    @Override
+    public List<? extends ACilkSpawn> selectCilkSpawn() {
+        return this.aStatement.selectCilkSpawn();
+    }
+
+    /**
+     * Replaces this join point with the given join
+     * @param node 
+     */
+    @Override
+    public AJoinPoint replaceWithImpl(AJoinPoint node) {
+        return this.aStatement.replaceWithImpl(node);
+    }
+
+    /**
+     * Overload which accepts a string
+     * @param node 
+     */
+    @Override
+    public AJoinPoint replaceWithImpl(String node) {
+        return this.aStatement.replaceWithImpl(node);
+    }
+
+    /**
+     * Inserts the given join point before this join point
+     * @param node 
+     */
+    @Override
+    public AJoinPoint insertBeforeImpl(AJoinPoint node) {
+        return this.aStatement.insertBeforeImpl(node);
+    }
+
+    /**
+     * Overload which accepts a string
+     * @param node 
+     */
+    @Override
+    public AJoinPoint insertBeforeImpl(String node) {
+        return this.aStatement.insertBeforeImpl(node);
+    }
+
+    /**
+     * Inserts the given join point after this join point
+     * @param node 
+     */
+    @Override
+    public AJoinPoint insertAfterImpl(AJoinPoint node) {
+        return this.aStatement.insertAfterImpl(node);
+    }
+
+    /**
+     * Overload which accepts a string
+     * @param code 
+     */
+    @Override
+    public AJoinPoint insertAfterImpl(String code) {
+        return this.aStatement.insertAfterImpl(code);
+    }
+
+    /**
+     * Removes the node associated to this joinpoint from the AST
+     */
+    @Override
+    public void detachImpl() {
+        this.aStatement.detachImpl();
+    }
+
+    /**
+     * Sets the type of a node, if it has a type
+     * @param type 
+     */
+    @Override
+    public void setTypeImpl(AJoinPoint type) {
+        this.aStatement.setTypeImpl(type);
+    }
+
+    /**
+     * Performs a copy of the node and its children, but not of the nodes in its fields
+     */
+    @Override
+    public AJoinPoint copyImpl() {
+        return this.aStatement.copyImpl();
+    }
+
+    /**
+     * Performs a copy of the node and its children, including the nodes in their fields (only the first level of field nodes, this function is not recursive)
+     */
+    @Override
+    public AJoinPoint deepCopyImpl() {
+        return this.aStatement.deepCopyImpl();
+    }
+
+    /**
+     * Associates arbitrary values to nodes of the AST
+     * @param fieldName 
+     * @param value 
+     */
+    @Override
+    public Object setUserFieldImpl(String fieldName, Object value) {
+        return this.aStatement.setUserFieldImpl(fieldName, value);
+    }
+
+    /**
+     * Overload which accepts a map
+     * @param fieldNameAndValue 
+     */
+    @Override
+    public Object setUserFieldImpl(Map<?, ?> fieldNameAndValue) {
+        return this.aStatement.setUserFieldImpl(fieldNameAndValue);
+    }
+
+    /**
+     * Sets the value associated with the given property key
+     * @param key 
+     * @param value 
+     */
+    @Override
+    public AJoinPoint setValueImpl(String key, Object value) {
+        return this.aStatement.setValueImpl(key, value);
+    }
+
+    /**
+     * Adds a message that will be printed to the user after weaving finishes. Identical messages are removed
+     * @param message 
+     */
+    @Override
+    public void messageToUserImpl(String message) {
+        this.aStatement.messageToUserImpl(message);
+    }
+
+    /**
+     * Removes the children of this node
+     */
+    @Override
+    public void removeChildrenImpl() {
+        this.aStatement.removeChildrenImpl();
+    }
+
+    /**
+     * Replaces the first child, or inserts the join point if no child is present
+     * @param node 
+     */
+    @Override
+    public void setFirstChildImpl(AJoinPoint node) {
+        this.aStatement.setFirstChildImpl(node);
+    }
+
+    /**
+     * Replaces the last child, or inserts the join point if no child is present
+     * @param node 
+     */
+    @Override
+    public void setLastChildImpl(AJoinPoint node) {
+        this.aStatement.setLastChildImpl(node);
+    }
+
+    /**
+     * 
+     * @param position 
+     * @param code 
+     */
+    @Override
+    public AJoinPoint[] insertImpl(String position, String code) {
+        return this.aStatement.insertImpl(position, code);
+    }
+
+    /**
+     * 
+     * @param position 
+     * @param code 
+     */
+    @Override
+    public AJoinPoint[] insertImpl(String position, JoinPoint code) {
+        return this.aStatement.insertImpl(position, code);
+    }
+
+    /**
+     * 
+     */
+    @Override
+    public String toString() {
+        return this.aStatement.toString();
+    }
+
+    /**
+     * 
+     */
+    @Override
+    public Optional<? extends AStatement> getSuper() {
+        return Optional.of(this.aStatement);
+    }
+
+    /**
+     * 
+     */
+    @Override
+    public final List<? extends JoinPoint> select(String selectName) {
+        List<? extends JoinPoint> joinPointList;
+        switch(selectName) {
+        	case "expr": 
+        		joinPointList = selectExpr();
+        		break;
+        	case "childExpr": 
+        		joinPointList = selectChildExpr();
+        		break;
+        	case "call": 
+        		joinPointList = selectCall();
+        		break;
+        	case "stmtCall": 
+        		joinPointList = selectStmtCall();
+        		break;
+        	case "memberCall": 
+        		joinPointList = selectMemberCall();
+        		break;
+        	case "memberAccess": 
+        		joinPointList = selectMemberAccess();
+        		break;
+        	case "arrayAccess": 
+        		joinPointList = selectArrayAccess();
+        		break;
+        	case "vardecl": 
+        		joinPointList = selectVardecl();
+        		break;
+        	case "varref": 
+        		joinPointList = selectVarref();
+        		break;
+        	case "op": 
+        		joinPointList = selectOp();
+        		break;
+        	case "binaryOp": 
+        		joinPointList = selectBinaryOp();
+        		break;
+        	case "unaryOp": 
+        		joinPointList = selectUnaryOp();
+        		break;
+        	case "newExpr": 
+        		joinPointList = selectNewExpr();
+        		break;
+        	case "deleteExpr": 
+        		joinPointList = selectDeleteExpr();
+        		break;
+        	case "cilkSpawn": 
+        		joinPointList = selectCilkSpawn();
+        		break;
+        	default:
+        		joinPointList = this.aStatement.select(selectName);
+        		break;
+        }
+        return joinPointList;
+    }
+
+    /**
+     * 
+     */
+    @Override
+    public final void defImpl(String attribute, Object value) {
+        switch(attribute){
+        case "type": {
+        	if(value instanceof AJoinPoint){
+        		this.defTypeImpl((AJoinPoint)value);
+        		return;
+        	}
+        	this.unsupportedTypeForDef(attribute, value);
+        }
+        case "firstChild": {
+        	if(value instanceof AJoinPoint){
+        		this.defFirstChildImpl((AJoinPoint)value);
+        		return;
+        	}
+        	this.unsupportedTypeForDef(attribute, value);
+        }
+        case "lastChild": {
+        	if(value instanceof AJoinPoint){
+        		this.defLastChildImpl((AJoinPoint)value);
+        		return;
+        	}
+        	this.unsupportedTypeForDef(attribute, value);
+        }
+        default: throw new UnsupportedOperationException("Join point "+get_class()+": attribute '"+attribute+"' cannot be defined");
+        }
+    }
+
+    /**
+     * 
+     */
+    @Override
+    protected final void fillWithAttributes(List<String> attributes) {
+        this.aStatement.fillWithAttributes(attributes);
+    }
+
+    /**
+     * 
+     */
+    @Override
+    protected final void fillWithSelects(List<String> selects) {
+        this.aStatement.fillWithSelects(selects);
+    }
+
+    /**
+     * 
+     */
+    @Override
+    protected final void fillWithActions(List<String> actions) {
+        this.aStatement.fillWithActions(actions);
+    }
+
+    /**
+     * Returns the join point type of this class
+     * @return The join point type
+     */
+    @Override
+    public final String get_class() {
+        return "case";
+    }
+
+    /**
+     * Defines if this joinpoint is an instanceof a given joinpoint class
+     * @return True if this join point is an instanceof the given class
+     */
+    @Override
+    public final boolean instanceOf(String joinpointClass) {
+        boolean isInstance = get_class().equals(joinpointClass);
+        if(isInstance) {
+        	return true;
+        }
+        return this.aStatement.instanceOf(joinpointClass);
+    }
+    /**
+     * 
+     */
+    protected enum CaseAttributes {
+        ISFIRST("isFirst"),
+        ISLAST("isLast"),
+        PARENT("parent"),
+        ASTANCESTOR("astAncestor"),
+        AST("ast"),
+        DATA("data"),
+        HASCHILDREN("hasChildren"),
+        DESCENDANTSANDSELF("descendantsAndSelf"),
+        TYPE("type"),
+        ISCILK("isCilk"),
+        FILEPATH("filepath"),
+        SCOPENODES("scopeNodes"),
+        CHILDREN("children"),
+        FIRSTCHILD("firstChild"),
+        NUMCHILDREN("numChildren"),
+        ANCESTOR("ancestor"),
+        ASTCHILD("astChild"),
+        ASTNAME("astName"),
+        JPID("jpId"),
+        ASTID("astId"),
+        CONTAINS("contains"),
+        ASTISINSTANCE("astIsInstance"),
+        FILENAME("filename"),
+        JAVAFIELDS("javaFields"),
+        ASTPARENT("astParent"),
+        USERFIELD("userField"),
+        HASNODE("hasNode"),
+        CHILD("child"),
+        ENDLINE("endLine"),
+        ENDCOLUMN("endColumn"),
+        CODE("code"),
+        ISINSIDELOOPHEADER("isInsideLoopHeader"),
+        LINE("line"),
+        KEYS("keys"),
+        ISINSIDEHEADER("isInsideHeader"),
+        ASTNUMCHILDREN("astNumChildren"),
+        DESCENDANTS("descendants"),
+        ASTCHILDREN("astChildren"),
+        ISMACRO("isMacro"),
+        LASTCHILD("lastChild"),
+        ROOT("root"),
+        JAVAVALUE("javaValue"),
+        KEYTYPE("keyType"),
+        CHAINANCESTOR("chainAncestor"),
+        CHAIN("chain"),
+        JOINPOINTTYPE("joinpointType"),
+        CURRENTREGION("currentRegion"),
+        HASASTPARENT("hasAstParent"),
+        COLUMN("column"),
+        PARENTREGION("parentRegion"),
+        GETVALUE("getValue"),
+        FIRSTJP("firstJp"),
+        DEPTH("depth"),
+        JAVAFIELDTYPE("javaFieldType"),
+        LOCATION("location"),
+        GETUSERFIELD("getUserField"),
+        PRAGMAS("pragmas"),
+        HASPARENT("hasParent");
+        private String name;
+
+        /**
+         * 
+         */
+        private CaseAttributes(String name){
+            this.name = name;
+        }
+        /**
+         * Return an attribute enumeration item from a given attribute name
+         */
+        public static Optional<CaseAttributes> fromString(String name) {
+            return Arrays.asList(values()).stream().filter(attr -> attr.name.equals(name)).findAny();
+        }
+
+        /**
+         * Return a list of attributes in String format
+         */
+        public static List<String> getNames() {
+            return Arrays.asList(values()).stream().map(CaseAttributes::name).collect(Collectors.toList());
+        }
+
+        /**
+         * True if the enum contains the given attribute name, false otherwise.
+         */
+        public static boolean contains(String name) {
+            return fromString(name).isPresent();
+        }
+    }
+}
