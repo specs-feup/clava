5af1bc5775e6a6ac5b487a75d6c69bbc495066ff
João Bispo <joaobispo@gmail.com>
2020-09-16 17:50:48 +0100

    [ClavaWeaver] Adds C++ bench test 'LoicEx1'

---
+++ ClavaWeaver/test/pt/up/fe/specs/cxxweaver/tests/CxxBenchTest.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2016 SPeCS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package pt.up.fe.specs.cxxweaver.tests;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import pt.up.fe.specs.clava.language.Standard;
+import pt.up.fe.specs.cxxweaver.ClavaWeaverTester;
+import pt.up.fe.specs.util.SpecsSystem;
+
+public class CxxBenchTest {
+
+    @BeforeClass
+    public static void setupOnce() {
+        SpecsSystem.programStandardInit();
+        ClavaWeaverTester.clean();
+    }
+
+    @After
+    public void tearDown() {
+        ClavaWeaverTester.clean();
+    }
+
+    private static ClavaWeaverTester newTester() {
+        return new ClavaWeaverTester("clava/test/bench/", Standard.CXX11)
+                .setResultPackage("cpp/results")
+                .setSrcPackage("cpp/src");
+    }
+
+    @Test
+    public void testLoicEx1() {
+        newTester().test("LoicEx1.lara", "loic_ex1.cpp");
+    }
+
+}
