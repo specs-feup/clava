52c4852d302ed6600757fd9f98dc1ae8a14c0c4e
João Bispo <joaobispo@gmail.com>
2020-10-20 19:02:12 +0100

    [ClavaWeaver] Adds weaver C++ test 'Member'

---
+++ ClavaWeaver/resources/clava/test/weaver/cpp/src/member.cpp
@@ -0,0 +1,14 @@
+﻿class Box {
+   public:
+	int age = 0;
+	int getAge(void);
+ 
+  
+};
+
+// Member functions definitions
+int Box::getAge(void) {
+	Box box;
+	age = 3;
+   return age + box.age + box.getAge();
+}
