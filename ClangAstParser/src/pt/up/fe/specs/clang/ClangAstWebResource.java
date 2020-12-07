301079e68d990b49d6df640d4ca88fcd716fe7fb
João Bispo <joaobispo@gmail.com>
2020-12-07 20:50:51 +0000

    [ClangAstParser] Updates dumper to version v4.2.12

--- ClangAstParser/src/pt/up/fe/specs/clang/ClangAstWebResource.java
+++ ClangAstParser/src/pt/up/fe/specs/clang/ClangAstWebResource.java
@@ -34,16 +34,16 @@ public interface ClangAstWebResource {
     // WebResourceProvider LIBC_CXX_LINUX = create("libc_cxx_linux.zip", "v1.0");
     // WebResourceProvider LIBC_CXX_CENTOS6 = create("libc_cxx_centos6.zip", "v1.0");
 
-    WebResourceProvider WIN_EXE = create("windows/clang_ast.exe", "v4.2.11");
+    WebResourceProvider WIN_EXE = create("windows/clang_ast.exe", "v4.2.12");
     WebResourceProvider WIN_DLL1 = create("windows/libwinpthread-1.dll");
     WebResourceProvider WIN_DLL2 = create("windows/zlib1.dll");
 
-    WebResourceProvider LINUX_EXE = create("linux_ubuntu_18/clang_ast", "v4.2.11");
+    WebResourceProvider LINUX_EXE = create("linux_ubuntu_18/clang_ast", "v4.2.12");
 
-    WebResourceProvider LINUX_ARMV7_EXE = create("linux_ubuntu_14_armv7/clang_ast", "v4.2.11");
+    WebResourceProvider LINUX_ARMV7_EXE = create("linux_ubuntu_14_armv7/clang_ast", "v4.2.12");
 
-    WebResourceProvider CENTOS_EXE = create("centos7/clang_ast", "v4.2.11");
+    WebResourceProvider CENTOS_EXE = create("centos7/clang_ast", "v4.2.12");
 
-    WebResourceProvider MAC_OS_EXE = create("macos/clang_ast", "v4.2.11");
+    WebResourceProvider MAC_OS_EXE = create("macos/clang_ast", "v4.2.12");
 
 }
