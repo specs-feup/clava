3727a0f1d893fe94e3d10f457beb1162440a011e
João Bispo <joaobispo@gmail.com>
2020-12-29 01:25:09 +0000

    [ClangAstParser] Rename files of C++ test issue #9

--- ClangAstParser/test/eu/antarex/clang/parser/tests/CxxIssuesTest.java
+++ ClangAstParser/test/eu/antarex/clang/parser/tests/CxxIssuesTest.java
@@ -33,8 +33,8 @@ public class CxxIssuesTest {
     }
 
     @Test
-    public void testClavaIssue9() {
-        new CxxTester("issues/clava_issue9.h").test();
+    public void testClavaIssue09() {
+        new CxxTester("issues/clava_issue09.h").test();
     }
 
     @Test
