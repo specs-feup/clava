f14b6c3efdca6e2c1b34ba7a0b30b07caa930643
João Bispo <joaobispo@gmail.com>
2021-07-12 23:47:25 +0100

    Moves more enums to automatic generation

--- ClangAstDumper/enums_Decl_h.cpp
+++ ClangAstDumper/enums_Decl_h.cpp
@@ -1,2 +1,14 @@
 #include "ClangEnums.h"
 
+extern const std::string clava::INIT_STYLE[] = {
+        "CINIT",
+        "CALL_INIT",
+        "LIST_INIT",
+};
+
+extern const std::string clava::TLS_KIND[] = {
+        "NONE",
+        "STATIC",
+        "DYNAMIC",
+};
+
---
+++ ClangAstDumper/enums_ExprCXX_h.cpp
@@ -0,0 +1,15 @@
+#include "ClangEnums.h"
+
+extern const std::string clava::NEW_INIT_STYLE[] = {
+        "NO_INIT",
+        "CALL_INIT",
+        "LIST_INIT",
+};
+
+extern const std::string clava::CONSTRUCTION_KIND[] = {
+        "Complete",
+        "NonVirtualBase",
+        "VirtualBase",
+        "Delegating",
+};
+
