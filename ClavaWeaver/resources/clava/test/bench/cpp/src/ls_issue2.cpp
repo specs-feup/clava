e0dfded49d34a051c4e59a0ed85c4af230baf23b
João Bispo <joaobispo@gmail.com>
2021-07-17 20:16:53 +0100

    [ClavaWeaver] Adds C++ bench/issue 'LSIssue2'

---
+++ ClavaWeaver/resources/clava/test/bench/cpp/src/ls_issue2.cpp
@@ -0,0 +1,12 @@
+/*
+	Simple matrix multiplication example.
+*/
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <chrono>
+void test_matrix_mul() {
+}
+int main() {
+	test_matrix_mul();
+}	
