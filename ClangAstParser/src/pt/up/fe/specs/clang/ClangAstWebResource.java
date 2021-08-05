7dbb517ba5080dab0d1bff76ae679e53c01d926b
João Bispo <joaobispo@gmail.com>
2021-08-05 10:41:38 +0100

    [ClangAstParser] Updates web resources related to libc files

--- ClangAstParser/src/pt/up/fe/specs/clang/ClangAstWebResource.java
+++ ClangAstParser/src/pt/up/fe/specs/clang/ClangAstWebResource.java
@@ -25,12 +25,12 @@ public interface ClangAstWebResource {
         return WebResourceProvider.newInstance("http://specs.fe.up.pt/resources/clangast/", resourceUrl);
     }
 
-    WebResourceProvider BUILTIN_INCLUDES = create("clang_builtin_includes_v7.0.1.zip", "v1.0");
+    WebResourceProvider BUILTIN_INCLUDES = create("clang_builtin_includes_v12.0.zip", "v12.0");
     // WebResourceProvider LIBC_CXX = create("libcxx_7.0.1.zip", "v1.0");
-    WebResourceProvider LIBC_CXX = create("libc_cxx.zip", "v12.0");
+    WebResourceProvider LIBC_CXX = create("libc_cxx.zip", "v12.0.1");
 
     // WebResourceProvider LIBC_CXX_WINDOWS = create("libc_cxx_windows.zip", "v1.0");
-    WebResourceProvider LIBC_CXX_WINDOWS = create("libc_cxx_windows.zip", "v12.0");
+    WebResourceProvider LIBC_CXX_WINDOWS = create("libc_cxx_windows.zip", "v12.0.1");
 
     WebResourceProvider CUDA_LIB = create("cudalib.zip", "v1.0");
 
