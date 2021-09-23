d91cdb9cd98b843b0aa6b03ba21829b7ac5f5cb2
João Bispo <joaobispo@gmail.com>
2021-09-23 18:29:08 -0400

    [ClavaWrapper] Builds json with arguments

--- ClavaWrapper/main.cpp
+++ ClavaWrapper/main.cpp
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include <stdlib.h>
 #include <unistd.h>
+#include <libgearman/gearman.h>
 
 std::string escape_json(const std::string &s) {
     std::ostringstream o;
@@ -17,14 +18,47 @@ std::string escape_json(const std::string &s) {
     return o.str();
 }
 
-int main() {
+int main(int argc, char** argv) {
+
+    // Try to connect to clava server
+    
+    // If failed, try launching it
+    
+    // If failed again, quit
+
+
+
+    // Get current folder
 
     const int MAX_PATH_LEGTH = 4096;
     char currentExe[MAX_PATH_LEGTH];
 
-    readlink("/proc/self/exe");
-    //system("");
+    readlink("/proc/self/exe", currentExe, sizeof(currentExe));
+    
+    std::string currentExeStr(currentExe);
+    std::string currentFolder = currentExeStr.substr(0, currentExeStr.find_last_of("/"));
+    ///proc/self/exe 
+    
+    // Build command
+    std::string cmd = currentFolder;
+    cmd += "/clava-server";
+    
+    // Create json with arguments
+    std::string jsonArgs("[");
+    for(int i=1; i<argc; i++) {
+    	std::string arg(argv[i]);
+    	if(i != 1) {
+        	jsonArgs += ", ";	
+    	}
 
+	jsonArgs += "'";
+        jsonArgs += escape_json(arg);
+	jsonArgs += "'";        
+    }
+    jsonArgs += "]";
+    
+    //system("");
+    std::cout << "Command: " << jsonArgs << std::endl;
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
