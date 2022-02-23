b47c4eaaa798a74888580f0153467b763b693502
João Bispo <joaobispo@gmail.com>
2022-02-23 19:28:24 +0000

    [ClavaWeaver] Improves weaver test 'Graal'

--- ClavaWeaver/resources/clava/test/weaver/GraalTest.js
+++ ClavaWeaver/resources/clava/test/weaver/GraalTest.js
@@ -1,4 +1,14 @@
 laraImport("weaver.Query");
 
-println("Descendants: " + Query.root().ast);
+println("Root type: " + Query.root().joinPointType);
+println("Root: " + Query.root());
+println("Is join point: " + isJoinPoint(Query.root()));
+println("Is program: " + isJoinPoint(Query.root(), "program"));
 
+const root = Query.root();
+const $file = root.children[0];
+const $function = $file.children[0];
+println("First grandchild: " + $function.joinPointType);
+println("Ancestor: " + $function.ancestor("file").joinPointType);
+
+//println("Function: " + Query.search("function").first().joinPointType);
