d148159e6518b364c4f5ea0e34b6238cdf043cd0
João Bispo <joaobispo@gmail.com>
2022-02-26 22:04:11 +0000

    [ClavaWeaver] Improves weaver test 'Graal'

--- ClavaWeaver/resources/clava/test/weaver/GraalTest.js
+++ ClavaWeaver/resources/clava/test/weaver/GraalTest.js
@@ -8,7 +8,13 @@ println("Is program: " + isJoinPoint(Query.root(), "program"));
 const root = Query.root();
 const $file = root.children[0];
 const $function = $file.children[0];
+
 println("First grandchild: " + $function.joinPointType);
-println("Ancestor: " + $function.ancestor("file").joinPointType);
+println("Ancestor that exists: " + $function.ancestor("file").joinPointType);
+println("Ancestor that does not exist: " + $function.ancestor("expr"));
+
+for(const $f of Query.search("function")) {
+	println("Function name: " + $f.name);
+}
 
 //println("Function: " + Query.search("function").first().joinPointType);
