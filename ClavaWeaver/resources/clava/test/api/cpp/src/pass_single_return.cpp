91002744200e3fdba14a3cb34a632cf6f87a2a6d
João N. Matos <me@joaonmatos.com>
2022-04-05 17:52:14 +0100

    Add missing test file

--- ClavaWeaver/resources/clava/test/api/cpp/src/pass_single_return.cpp
+++ ClavaWeaver/resources/clava/test/api/cpp/src/pass_single_return.cpp
@@ -1,1 +1,44 @@
 
+#include <iostream>
+
+int single_return(double n)
+{
+    return n > 0.0 ? 1 : 0;
+}
+
+int multiple_return(double n)
+{
+    if (n < 0.01 && n > -0.01)
+    {
+        return 0;
+    }
+    if (n > 0.0)
+    {
+        return 1;
+    }
+    return -1;
+}
+
+int single_return_oob(double n)
+{
+    if (n > 0.0)
+        return 1;
+}
+
+void no_return(double n)
+{
+    if (n / 16.0 > 1.0)
+    {
+        std::cout << "Greater than 16" << std::endl;
+    }
+}
+
+void earlyReturn(double n)
+{
+    if (n / 16.0 > 1.0)
+    {
+        std::cout << "Greater than 16" << std::endl;
+        return;
+    }
+    std::cout << "Not greater than 16" << std::endl;
+}
