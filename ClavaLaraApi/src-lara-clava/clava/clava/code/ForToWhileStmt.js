e0bee3f3070bb17a59bebd65f6427bbd81168544
João N. Matos <me@joaonmatos.com>
2022-06-06 21:59:08 +0100

    feat(ClavaLaraApi): Return new scope of ForToWhileStmt

--- ClavaLaraApi/src-lara-clava/clava/clava/code/ForToWhileStmt.js
+++ ClavaLaraApi/src-lara-clava/clava/clava/code/ForToWhileStmt.js
@@ -1,13 +1,15 @@
 laraImport("clava.ClavaJoinPoints");
 
 function ForToWhileStmt($forStmt) {
-  $forStmt.replaceWith(
-    ClavaJoinPoints.scope([
-      $forStmt.init,
-      ClavaJoinPoints.whileStmt(
-        $forStmt.cond,
-        ClavaJoinPoints.scope([...$forStmt.scopeNodes, $forStmt.step])
-      ),
-    ])
-  );
+  const $scope = ClavaJoinPoints.scope([
+    $forStmt.init,
+    ClavaJoinPoints.whileStmt(
+      $forStmt.cond,
+      ClavaJoinPoints.scope([...$forStmt.scopeNodes, $forStmt.step])
+    ),
+  ]);
+
+  $forStmt.replaceWith(scope);
+
+  return $scope;
 }
