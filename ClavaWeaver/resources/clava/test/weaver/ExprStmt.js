5a2d1d260c91f621935149b22a0980ff1f3a0f6b
João Bispo <joaobispo@gmail.com>
2022-06-28 10:55:34 +0100

    Formatting changes

--- ClavaWeaver/resources/clava/test/weaver/ExprStmt.js
+++ ClavaWeaver/resources/clava/test/weaver/ExprStmt.js
@@ -1,8 +1,8 @@
-laraImport("weaver.Query")
+laraImport("weaver.Query");
 
-for(const $loop of Query.search("function", "loopExprStmt").search("loop")) {
-	println(`Loop ${$loop.line} cond: ${$loop.cond.code}`)
-	if($loop.step !== undefined) {
-		println(`Loop ${$loop.line} step: ${$loop.step.code}`)		
-	}
+for (const $loop of Query.search("function", "loopExprStmt").search("loop")) {
+  println(`Loop ${$loop.line} cond: ${$loop.cond.code}`);
+  if ($loop.step !== undefined) {
+    println(`Loop ${$loop.line} step: ${$loop.step.code}`);
+  }
 }
