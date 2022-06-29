2d5a5e6ad4b5c0d551dbdae38856d1aeb7314a0b
João N. Matos <me@joaonmatos.com>
2022-06-29 12:58:58 +0100

    fix(ClavaLaraApi): Fix ForToWhileStmt code
     * Fix uncaught syntax errors
     * Deal with presence of continues

--- ClavaLaraApi/src-lara-clava/clava/clava/code/ForToWhileStmt.js
+++ ClavaLaraApi/src-lara-clava/clava/clava/code/ForToWhileStmt.js
@@ -1,15 +1,47 @@
 laraImport("clava.ClavaJoinPoints");
 
-function ForToWhileStmt($forStmt) {
+function ForToWhileStmt($forStmt, stepLabelSuffix) {
+  // replace continues with gotos to the step statement for the new loop body
+  const localContinues = [...ForToWhileStmt._findLocalContinue($forStmt.body)];
+  let loopStepStatements;
+  if (localContinues.length > 0) {
+    const $labelDecl = ClavaJoinPoints.labelDecl(
+      `__for_loop_step_${stepLabelSuffix}`
+    );
+    loopStepStatements = [ClavaJoinPoints.labelStmt($labelDecl), $forStmt.step];
+
+    for (const $continue of localContinues) {
+      $continue.replaceWith(ClavaJoinPoints.gotoStmt($labelDecl));
+    }
+  } else {
+    loopStepStatements = [$forStmt.step];
+  }
+
   const $scope = ClavaJoinPoints.scope([
     $forStmt.init,
     ClavaJoinPoints.whileStmt(
       $forStmt.cond,
-      ClavaJoinPoints.scope([...$forStmt.scopeNodes, $forStmt.step])
+      ClavaJoinPoints.scope([...$forStmt.scopeNodes, ...loopStepStatements])
     ),
   ]);
 
-  $forStmt.replaceWith(scope);
+  $forStmt.replaceWith($scope);
 
   return $scope;
 }
+
+// find local continue statements in a loop: that is, recursively traverse the tree and return continue statements,
+// while ignoring sub-trees starting from an inner loop
+// TODO: optimization - ignore sub-trees that will for sure not contain ContinueStmt nodes
+ForToWhileStmt._findLocalContinue = function* ($jp) {
+  if ($jp.astName === "ContinueStmt") {
+    yield $jp;
+    return;
+  }
+  if ($jp.instanceOf("loop")) {
+    return;
+  }
+  for (const $child of $jp.children) {
+    yield* ForToWhileStmt._findLocalContinue($child);
+  }
+};
