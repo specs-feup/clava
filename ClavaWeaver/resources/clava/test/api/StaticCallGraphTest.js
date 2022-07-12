8593d4eb6b9c2230d6daa4d8db5497187cf8de21
João Bispo <joaobispo@gmail.com>
2022-07-12 21:03:29 +0100

    [ClavaWeaver] Updates C API test 'StaticCallGraph'

--- ClavaWeaver/resources/clava/test/api/StaticCallGraphTest.js
+++ ClavaWeaver/resources/clava/test/api/StaticCallGraphTest.js
@@ -4,14 +4,14 @@ var fullGraph = StaticCallGraph.build().graph;
 println("Full graph");
 println(Graphs.toDot(fullGraph));
 
-var functionGraph = StaticCallGraph.build(Query.search("function", "foo1").first()).graph;
+var functionGraph = StaticCallGraph.build(Query.search("function", "foo1").first(), false).graph;
 println("Function graph");
 println(Graphs.toDot(functionGraph));
 
-var functionGraphFull = StaticCallGraph.build(Query.search("function", "foo1").first(), true).graph;
+var functionGraphFull = StaticCallGraph.build(Query.search("function", "foo1").first()).graph;
 println("Function graph full");
 println(Graphs.toDot(functionGraphFull));
 
-var callGraphFull = StaticCallGraph.build(Query.search("function", "main").search("call", "foo1").first(), true).graph;
+var callGraphFull = StaticCallGraph.build(Query.search("function", "main").search("call", "foo1").first()).graph;
 println("Function graph full");
 println(Graphs.toDot(callGraphFull));
