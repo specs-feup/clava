d3806733fffab69500aad4fc56b703828679674e
João Bispo <joaobispo@gmail.com>
2022-07-12 22:38:18 +0100

    [ClavaWeaver] Updates C API test 'StaticCallGraph'

--- ClavaWeaver/resources/clava/test/api/StaticCallGraphTest.js
+++ ClavaWeaver/resources/clava/test/api/StaticCallGraphTest.js
@@ -7,26 +7,26 @@ dotFormatter.addNodeAttribute("style=dashed", node => Graphs.isLeafNode(node) &&
 dotFormatter.addNodeAttribute("style=filled", node => Graphs.isLeafNode(node) && node.data().hasCalls());
  
 
-var fullGraph = StaticCallGraph.build().graph;
+var fullGraph = StaticCallGraph.build();
 println("Full graph");
-println(Graphs.toDot(fullGraph, dotFormatter));
+println(fullGraph.toDot());
 
-var functionGraph = StaticCallGraph.build(Query.search("function", "foo1").first(), false).graph;
+var functionGraph = StaticCallGraph.build(Query.search("function", "foo1").first(), false);
 println("Function graph");
-println(Graphs.toDot(functionGraph, dotFormatter));
+println(functionGraph.toDot());
 
-var functionGraphFull = StaticCallGraph.build(Query.search("function", "foo1").first()).graph;
+var functionGraphFull = StaticCallGraph.build(Query.search("function", "foo1").first());
 println("Function graph full");
-println(Graphs.toDot(functionGraphFull, dotFormatter));
+println(functionGraphFull.toDot());
 
-var callGraphFull = StaticCallGraph.build(Query.search("function", "main").search("call", "foo1").first()).graph;
+var callGraphFull = StaticCallGraph.build(Query.search("function", "main").search("call", "foo1").first());
 println("Function graph full");
-println(Graphs.toDot(callGraphFull, dotFormatter));
+println(callGraphFull.toDot());
 
 // Get leaf nodes
-const fullLeafNodes = fullGraph.nodes().filter(node => Graphs.isLeafNode(node));
+const fullLeafNodes = fullGraph.graph.nodes().filter(node => Graphs.isLeafNode(node));
 println("Full graph leaf nodes: " + fullLeafNodes.map(node => node.data()))
 
-const functionLeafNodes = functionGraph.nodes().filter(node => Graphs.isLeafNode(node));
+const functionLeafNodes = functionGraph.graph.nodes().filter(node => Graphs.isLeafNode(node));
 println("Function graph leaf nodes: " + functionLeafNodes.map(node => node.data()))
 println("Function graph leaf nodes with implementation: " + functionLeafNodes.filter(node => node.data().hasCalls()).map(node => node.data()))
