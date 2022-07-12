a2a8d32826a46bb910394209327a4169e4f19f16
João Bispo <joaobispo@gmail.com>
2022-07-12 20:53:23 +0100

    [ClavaLaraApi] Adds ScgNodeData.hasImplementation()

--- ClavaLaraApi/src-lara-clava/clava/clava/graphs/scg/ScgNodeData.js
+++ ClavaLaraApi/src-lara-clava/clava/clava/graphs/scg/ScgNodeData.js
@@ -1,20 +1,28 @@
 laraImport("lara.graphs.NodeData");
 
 class ScgNodeData extends NodeData {
-	
-	// The function represented by this node
-	#function;
+  // The function represented by this node
+  #function;
 
-	constructor($function) {
-		super();
-		this.#function = $function;
-	}	
+  constructor($function) {
+    super();
 
-	get function() {
-		return this.#function;
-	}	
-	
-	toString() {
-		return this.#function.signature;
-	}
+    // Should use only canonical functions
+    this.#function = $function.canonical;
+  }
+
+  get function() {
+    return this.#function;
+  }
+
+  toString() {
+    return this.#function.signature;
+  }
+
+  /**
+   * @returns true, if the function represented by this node has an available implementation, false otherwise
+   */
+  hasImplementation() {
+    return this.#function.isImplementation;
+  }
 }
