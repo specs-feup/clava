f9a5456d47a127678041ad5ffd9f69d88f8d2d16
João Bispo <joaobispo@gmail.com>
2022-07-12 20:43:41 +0100

    [ClavaLaraApi] Changes default value of visitCalls in StaticCallGraph.build() to true

--- ClavaLaraApi/src-lara-clava/clava/clava/graphs/StaticCallGraph.js
+++ ClavaLaraApi/src-lara-clava/clava/clava/graphs/StaticCallGraph.js
@@ -5,37 +5,41 @@ laraImport("lara.util.StringSet");
 laraImport("clava.graphs.scg.StaticCallGraphBuilder");
 
 class StaticCallGraph {
-	
-	// The static call graph
-	#graph;
-	
-	// Maps functions to graph nodes
-	#functions;
-	
-	constructor(graph, functions) {
-		this.#graph = graph;
-		this.#functions = functions;
-	}
-	
-	static build($jp, visitCalls = false) {
-		const builder = new StaticCallGraphBuilder();
-		
-		const graph = builder.build($jp, visitCalls);
+  // The static call graph
+  #graph;
 
-		return new StaticCallGraph(graph, builder.nodes);
-	}
-	
-	get graph() {
-		return this.#graph;
-	}
-	
-	get functions() {
-		return this.#functions;
-	}
-	
-	getNode($function) {
-		// Normalize function 
-		return this.#functions[$function.canonical.astId];
-	}	
+  // Maps functions to graph nodes
+  #functions;
 
+  constructor(graph, functions) {
+    this.#graph = graph;
+    this.#functions = functions;
+  }
+
+  /**
+   *
+   * @param {$jp} $jp
+   * @param {boolean} [visitCalls = true] - If true, recursively visits the functions of each call, building a call graph of the available code
+   * @returns
+   */
+  static build($jp, visitCalls = true) {
+    const builder = new StaticCallGraphBuilder();
+
+    const graph = builder.build($jp, visitCalls);
+
+    return new StaticCallGraph(graph, builder.nodes);
+  }
+
+  get graph() {
+    return this.#graph;
+  }
+
+  get functions() {
+    return this.#functions;
+  }
+
+  getNode($function) {
+    // Normalize function
+    return this.#functions[$function.canonical.astId];
+  }
 }
--- ClavaLaraApi/src-lara-clava/clava/clava/graphs/scg/StaticCallGraphBuilder.js
+++ ClavaLaraApi/src-lara-clava/clava/clava/graphs/scg/StaticCallGraphBuilder.js
@@ -8,152 +8,160 @@ laraImport("clava.graphs.scg.ScgNodeData");
 laraImport("clava.graphs.scg.ScgEdgeData");
 
 class StaticCallGraphBuilder {
-	
-	// The static call graph
-	#graph;
-	
-	// Maps AST nodes to graph nodes
-	#nodes;
-	
-	// Maps function->function relations to edges
-	#edges;
-	
-	#nodeCounter;
-	
-	constructor() {
-		this.#graph = Graphs.newGraph();
-		this.#nodes = {};
-		this.#edges = {};
-		this.#nodeCounter = 0;
-	}
-	
-	get nodes() {
-		return this.#nodes;
-	}
-	
-	#addOrGetNode($function) {
-		// Only functions can be nodes
-		Check.isJoinPoint($function, "function");
-		
-		let graphNode = this.#nodes[$function.astId];
-		
-		// If not found, create and add it
-		if(graphNode === undefined) {
-			const nodeData = new ScgNodeData($function);
-			nodeData.id = this.#newNodeId();
-			graphNode = Graphs.addNode(this.#graph, nodeData);
-			this.#nodes[$function.astId] = graphNode;
-		}
-		
-		return graphNode;
-	}
-	
-	#newNodeId() {
-		const id = "node_" + this.#nodeCounter;
-		this.#nodeCounter++;
-		return id;
-	}
+  // The static call graph
+  #graph;
 
-	
-	build($jp, visitCalls = false) {
-		// If undefined, assume root
-		if($jp === undefined) {
-			$jp = Query.root();
-		}
+  // Maps AST nodes to graph nodes
+  #nodes;
 
+  // Maps function->function relations to edges
+  #edges;
 
-		// Get function->call relations
-		let pairs = this.#getFunctionCallPairs($jp, visitCalls);
-		
-		// Add nodes and edges
-		for(const pair of pairs) {
-			const $sourceFunction = pair["function"];
-			const $call = pair["call"];			
-			const $targetFunction = $call.function;	// Already canonical function					
-			
-			const sourceNode = this.#addOrGetNode($sourceFunction);
-			const targetNode = this.#addOrGetNode($targetFunction);			
-			
-			this.#addEdge(sourceNode, targetNode, $call);			
-		}
+  #nodeCounter;
 
-		return this.#graph;
-	}
+  constructor() {
+    this.#graph = Graphs.newGraph();
+    this.#nodes = {};
+    this.#edges = {};
+    this.#nodeCounter = 0;
+  }
 
-	static getEdgeId(sourceNode, targetNode) {
-		return sourceNode.data().function.signature + "$" + targetNode.data().function.signature;
-	}
+  get nodes() {
+    return this.#nodes;
+  }
 
-	#addEdge(sourceNode, targetNode, $call) {
-		const edgeId = StaticCallGraphBuilder.getEdgeId(sourceNode, targetNode);
-		//println("Source->Target Id: " + sourceTargetId);
-		
-		let edge = this.#edges[edgeId];
-		if(edge === undefined) {
-			const edgeData = new ScgEdgeData();
-			edgeData.id = edgeId;
-			edge = Graphs.addEdge(this.#graph, sourceNode, targetNode, edgeData);				
-			this.#edges[edgeId] = edge;
-		}
-		
-		// Increment edge value
-		edge.data().inc($call);
-	}
+  #addOrGetNode($function) {
+    // Only functions can be nodes
+    Check.isJoinPoint($function, "function");
 
+    let graphNode = this.#nodes[$function.astId];
 
-	
-	#getFunctionCallPairs($jp, visitCalls) {
-		let pairs = [];
-		let seenFunctions = new StringSet(); 
-		let jpsToSearch = [$jp];
+    // If not found, create and add it
+    if (graphNode === undefined) {
+      const nodeData = new ScgNodeData($function);
+      nodeData.id = this.#newNodeId();
+      graphNode = Graphs.addNode(this.#graph, nodeData);
+      this.#nodes[$function.astId] = graphNode;
+    }
 
-		while(jpsToSearch.length > 0) {			
-			
-			let seenCalls = [];
-			
-			// Check if any of the jps to search is a call
-			for(const $jp of jpsToSearch) {
-				if($jp.instanceOf("call")) {
-					seenCalls.push($jp);
-				}
-			}
-			
-			for(const $jpToSearch of jpsToSearch) {
-			
-				// Get all function/call pairs for functions that are not yet in the seen functions
-				let functionCall = Query.searchFromInclusive($jpToSearch, "function", self => !seenFunctions.has(self.signature)).search("call").chain();		
-			
-				for(const pair of functionCall) {
-					seenFunctions.add(pair["function"].signature);
-					pairs.push({function: pair["function"], call: pair["call"] });
-					seenCalls.push(pair["call"]);
-				}
-			}
-			
-			// Calculate new jps to search
-			jpsToSearch = [];
-			if(visitCalls) {
-				for(const $call of seenCalls) {
-					const $functionDef = $call.definition;
-					
-					// Only visit if there is a definition					
-					if($functionDef === undefined) {
-						continue;
-					}
-					
-					// Check if already visited
-					if(seenFunctions.has($functionDef.signature)) {
-						continue;
-					}
-					
-					// Add function
-					jpsToSearch.push($functionDef);	
-					//println("Adding function " + $functionDef.signature)	
-				}					
-			}
-		}
-		
-		return pairs;		
-	}
+    return graphNode;
+  }
+
+  #newNodeId() {
+    const id = "node_" + this.#nodeCounter;
+    this.#nodeCounter++;
+    return id;
+  }
+
+  /**
+   *
+   * @param {$jp} $jp
+   * @param {boolean} [visitCalls = true] - If true, recursively visits the functions of each call, building a call graph of the available code
+   * @returns
+   */
+  build($jp, visitCalls = true) {
+    // If undefined, assume root
+    if ($jp === undefined) {
+      $jp = Query.root();
+    }
+
+    // Get function->call relations
+    let pairs = this.#getFunctionCallPairs($jp, visitCalls);
+
+    // Add nodes and edges
+    for (const pair of pairs) {
+      const $sourceFunction = pair["function"];
+      const $call = pair["call"];
+      const $targetFunction = $call.function; // Already canonical function
+
+      const sourceNode = this.#addOrGetNode($sourceFunction);
+      const targetNode = this.#addOrGetNode($targetFunction);
+
+      this.#addEdge(sourceNode, targetNode, $call);
+    }
+
+    return this.#graph;
+  }
+
+  static getEdgeId(sourceNode, targetNode) {
+    return (
+      sourceNode.data().function.signature +
+      "$" +
+      targetNode.data().function.signature
+    );
+  }
+
+  #addEdge(sourceNode, targetNode, $call) {
+    const edgeId = StaticCallGraphBuilder.getEdgeId(sourceNode, targetNode);
+    //println("Source->Target Id: " + sourceTargetId);
+
+    let edge = this.#edges[edgeId];
+    if (edge === undefined) {
+      const edgeData = new ScgEdgeData();
+      edgeData.id = edgeId;
+      edge = Graphs.addEdge(this.#graph, sourceNode, targetNode, edgeData);
+      this.#edges[edgeId] = edge;
+    }
+
+    // Increment edge value
+    edge.data().inc($call);
+  }
+
+  #getFunctionCallPairs($jp, visitCalls) {
+    let pairs = [];
+    let seenFunctions = new StringSet();
+    let jpsToSearch = [$jp];
+
+    while (jpsToSearch.length > 0) {
+      let seenCalls = [];
+
+      // Check if any of the jps to search is a call
+      for (const $jp of jpsToSearch) {
+        if ($jp.instanceOf("call")) {
+          seenCalls.push($jp);
+        }
+      }
+
+      for (const $jpToSearch of jpsToSearch) {
+        // Get all function/call pairs for functions that are not yet in the seen functions
+        let functionCall = Query.searchFromInclusive(
+          $jpToSearch,
+          "function",
+          (self) => !seenFunctions.has(self.signature)
+        )
+          .search("call")
+          .chain();
+
+        for (const pair of functionCall) {
+          seenFunctions.add(pair["function"].signature);
+          pairs.push({ function: pair["function"], call: pair["call"] });
+          seenCalls.push(pair["call"]);
+        }
+      }
+
+      // Calculate new jps to search
+      jpsToSearch = [];
+      if (visitCalls) {
+        for (const $call of seenCalls) {
+          const $functionDef = $call.definition;
+
+          // Only visit if there is a definition
+          if ($functionDef === undefined) {
+            continue;
+          }
+
+          // Check if already visited
+          if (seenFunctions.has($functionDef.signature)) {
+            continue;
+          }
+
+          // Add function
+          jpsToSearch.push($functionDef);
+          //println("Adding function " + $functionDef.signature)
+        }
+      }
+    }
+
+    return pairs;
+  }
 }
-
