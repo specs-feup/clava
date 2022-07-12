fa9cfd1e92a97e564c7065d7c3c95a6f8dda246b
João Bispo <joaobispo@gmail.com>
2022-07-12 21:42:16 +0100

    [ClavaWeaver] Updates C test 'StaticCallGraph'

--- ClavaWeaver/resources/clava/test/api/StaticCallGraphTest.js
+++ ClavaWeaver/resources/clava/test/api/StaticCallGraphTest.js
@@ -1,17 +1,32 @@
 laraImport("clava.graphs.StaticCallGraph");
+laraImport("lara.graphs.Graphs");
+laraImport("lara.graphs.DotFormatter");
+
+const dotFormatter = new DotFormatter();
+dotFormatter.addNodeAttribute("style=dashed", node => Graphs.isLeafNode(node) && !node.data().hasImplementation());
+dotFormatter.addNodeAttribute("style=filled", node => Graphs.isLeafNode(node) && node.data().hasCalls());
+ 
 
 var fullGraph = StaticCallGraph.build().graph;
 println("Full graph");
-println(Graphs.toDot(fullGraph));
+println(Graphs.toDot(fullGraph, dotFormatter));
 
 var functionGraph = StaticCallGraph.build(Query.search("function", "foo1").first(), false).graph;
 println("Function graph");
-println(Graphs.toDot(functionGraph));
+println(Graphs.toDot(functionGraph, dotFormatter));
 
 var functionGraphFull = StaticCallGraph.build(Query.search("function", "foo1").first()).graph;
 println("Function graph full");
-println(Graphs.toDot(functionGraphFull));
+println(Graphs.toDot(functionGraphFull, dotFormatter));
 
 var callGraphFull = StaticCallGraph.build(Query.search("function", "main").search("call", "foo1").first()).graph;
 println("Function graph full");
-println(Graphs.toDot(callGraphFull));
+println(Graphs.toDot(callGraphFull, dotFormatter));
+
+// Get leaf nodes
+const fullLeafNodes = fullGraph.nodes().filter(node => Graphs.isLeafNode(node));
+println("Full graph leaf nodes: " + fullLeafNodes.map(node => node.data()))
+
+const functionLeafNodes = functionGraph.nodes().filter(node => Graphs.isLeafNode(node));
+println("Function graph leaf nodes: " + functionLeafNodes.map(node => node.data()))
+println("Function graph leaf nodes with implementation: " + functionLeafNodes.filter(node => node.data().hasCalls()).map(node => node.data()))
