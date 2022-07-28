98c06e8e2ef15c4b3f1ca5e453ca1dd92a43ea4f
João Bispo <joaobispo@gmail.com>
2022-07-28 23:06:42 +0100

    [ClavaLaraApi] Inlining supports an option object, and 'normalizeToSubset' option

--- ClavaLaraApi/src-lara-clava/clava/clava/opt/Inlining.js
+++ ClavaLaraApi/src-lara-clava/clava/clava/opt/Inlining.js
@@ -3,8 +3,16 @@ laraImport("clava.opt.NormalizeToSubset");
 laraImport("clava.opt.PrepareForInlining");
 laraImport("clava.code.Inliner");
 
-function Inlining() {
-  NormalizeToSubset(Query.root());
+/**
+ *
+ * @param  {object} options - Object with options. Supported options: 'normalizeToSubset' (default: {}), options for function NormalizeToSubset
+ */
+function Inlining(options) {
+  _options = options ?? {};
+  _options["normalizeToSubset"] ??= {};
+
+  // TODO: Maybe passing a NormalizeToSubset instance is preferrable, but that means making NormalizeToSubset a class instead of a function
+  NormalizeToSubset(Query.root(), _options["normalizeToSubset"]);
 
   const inliner = new Inliner();
 
