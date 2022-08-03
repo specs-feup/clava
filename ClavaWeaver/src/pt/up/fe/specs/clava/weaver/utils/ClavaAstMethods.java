f73c0b2c1af5a6b05ff70c8103018f247e45cd9a
João Bispo <joaobispo@gmail.com>
2022-08-03 19:28:05 +0100

    [ClavaWeaver] Disables replacement of method prototypes with implementations
    - This is already being handled in Selector._addJps, and produces unwanted results (i.e. if there is both a prototype and an implementations, returns both, as implementations);

--- ClavaWeaver/src/pt/up/fe/specs/clava/weaver/utils/ClavaAstMethods.java
+++ ClavaWeaver/src/pt/up/fe/specs/clava/weaver/utils/ClavaAstMethods.java
@@ -24,10 +24,8 @@ import org.lara.interpreter.weaver.interf.WeaverEngine;
 
 import pt.up.fe.specs.clava.ClavaNode;
 import pt.up.fe.specs.clava.ast.decl.CXXMethodDecl;
-import pt.up.fe.specs.clava.ast.decl.CXXRecordDecl;
 import pt.up.fe.specs.clava.ast.decl.FunctionDecl;
 import pt.up.fe.specs.clava.utils.NullNode;
-import pt.up.fe.specs.util.SpecsCheck;
 
 public class ClavaAstMethods extends TreeNodeAstMethods<ClavaNode> {
 
@@ -41,8 +39,8 @@ public class ClavaAstMethods extends TreeNodeAstMethods<ClavaNode> {
     @Override
     protected Object[] getChildrenImpl(ClavaNode node) {
 
-        final List<Object> childrenList = Arrays.asList(node.getChildren());
-        
+        // final List<Object> childrenList = Arrays.asList(node.getChildren());
+
         var children = node.getChildren().stream()
                 // Filter null nodes
                 .filter(child -> !(child instanceof NullNode))
@@ -51,14 +49,19 @@ public class ClavaAstMethods extends TreeNodeAstMethods<ClavaNode> {
                 .toArray();
 
         /* */
+
+        // This is already being handled in Selector._addJps
+        /*
         if (node instanceof CXXRecordDecl) {
             replaceForMethodImplementations(children);
         }
+        */
+
         /* */
 
         return children;
     }
-    
+
     /**
      * Because we are filtering null nodes, the number of children must be adjusted
      */
@@ -67,61 +70,50 @@ public class ClavaAstMethods extends TreeNodeAstMethods<ClavaNode> {
         return getChildrenImpl(node).length;
     }
 
+    /*
     protected static boolean lclFilter(ClavaNode node) {
         // Function
         if (node instanceof FunctionDecl) {
             var functionDecl = (FunctionDecl) node;
             var declaration = functionDecl.getPrototypes();
             var definition = functionDecl.getImplementation();
-
+    
             SpecsCheck.checkArgument(!declaration.isEmpty() || definition.isPresent(),
                     () -> "Expected at least one of them to be present");
-
+    
             // XOR, if only one of them is present, current node must be one of them
             if (declaration.isEmpty() ^ definition.isEmpty()) {
                 return true;
             }
-
+    
             // Both are present, only return current node if it is the definition
             // Using == since they must be the same object
             return definition.get() == node;
         }
-
-        /*
-        //  Classes
-        if (node instanceof CXXRecordDecl) {
-        
-            if (node.get(TagDecl.IS_COMPLETE_DEFINITION))
-                return true;
-        
-            var definition = node.get(CXXRecordDecl.RECORD_DEFINITION);
-            if (definition.isPresent() && definition.get() == node)
-                return false;
-        
-            else
-                return true;
-        
-        }
-        */
-
+    
+      
+    
         // No other cases
         return true;
     }
-    
+    */
     protected static Object replaceMethodImplementation(Object child, List<Object> childrenList) {
-            
+
         // check if CXXMethodDecl
-        if (!(child instanceof CXXMethodDecl)) return child;
-        
+        if (!(child instanceof CXXMethodDecl))
+            return child;
+
         CXXMethodDecl method = (CXXMethodDecl) child;
-        
+
         // if has body, do nothing
-        if (method.hasBody()) return child;
-        
+        if (method.hasBody())
+            return child;
+
         // if implementation is equal, do nothing
         Optional<FunctionDecl> implementationOptional = method.getImplementation();
-        if (!implementationOptional.isPresent() || method.equals(implementationOptional.get())) return child; 
-        
+        if (!implementationOptional.isPresent() || method.equals(implementationOptional.get()))
+            return child;
+
         FunctionDecl implementation = implementationOptional.get();
 
         boolean isAlreadyInMethodsList = childrenList.stream()
@@ -137,24 +129,25 @@ public class ClavaAstMethods extends TreeNodeAstMethods<ClavaNode> {
                 // map to code
                 .map(impl -> impl.get().getCode())
                 .anyMatch(code -> code.equals(implementation.getCode()));
-        
-        if (isAlreadyInMethodsList) return child;
+
+        if (isAlreadyInMethodsList)
+            return child;
 
         // System.out.println("replacing impl for => " + node.getDeclName() + "." + method.getDeclName());
-        
+
         return implementation;
     }
-    
+
     protected static Object[] replaceForMethodImplementations(Object[] children) {
-        
+
         List<Object> childrenList = Arrays.asList(children);
 
         // with index in order to replace at position
-        for (int i = 0 ; i < children.length ; i++) {
+        for (int i = 0; i < children.length; i++) {
             Object child = children[i];
-            
+
             children[i] = replaceMethodImplementation(child, childrenList);
-            
+
             /*
             if (!(child instanceof CXXMethodDecl)) continue;
             
@@ -168,7 +161,7 @@ public class ClavaAstMethods extends TreeNodeAstMethods<ClavaNode> {
             if (!implementationOptional.isPresent() || method.equals(implementationOptional.get())) continue; 
             
             FunctionDecl implementation = implementationOptional.get();
-
+            
             boolean isAlreadyInMethodsList = childrenList.stream()
                     // filter non-methods
                     .filter(obj -> obj instanceof CXXMethodDecl)
@@ -184,15 +177,14 @@ public class ClavaAstMethods extends TreeNodeAstMethods<ClavaNode> {
                     .anyMatch(code -> code.equals(implementation.getCode()));
             
             if (isAlreadyInMethodsList) continue;
-
+            
             children[i] = implementation;
-
+            
             // System.out.println("replacing impl for => " + node.getDeclName() + "." + method.getDeclName());
              */
         }
-        
+
         return children;
     }
-
 
 }
