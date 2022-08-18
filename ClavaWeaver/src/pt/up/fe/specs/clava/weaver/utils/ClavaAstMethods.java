b167ffe3964a5df314d62fd54de3d7189edcb5f6
João Bispo <joaobispo@gmail.com>
2022-08-18 15:06:02 +0100

    Moves functionality from CommonJoinPoints to ClavaAstMethods

--- ClavaWeaver/src/pt/up/fe/specs/clava/weaver/utils/ClavaAstMethods.java
+++ ClavaWeaver/src/pt/up/fe/specs/clava/weaver/utils/ClavaAstMethods.java
@@ -22,23 +22,31 @@ import org.lara.interpreter.weaver.interf.WeaverEngine;
 
 import pt.up.fe.specs.clava.ClavaNode;
 import pt.up.fe.specs.clava.ast.decl.CXXMethodDecl;
+import pt.up.fe.specs.clava.ast.decl.CXXRecordDecl;
 import pt.up.fe.specs.clava.ast.decl.FunctionDecl;
 import pt.up.fe.specs.clava.ast.decl.RecordDecl;
 import pt.up.fe.specs.clava.utils.NullNode;
+import pt.up.fe.specs.util.classmap.FunctionClassMap;
 
 public class ClavaAstMethods extends TreeNodeAstMethods<ClavaNode> {
 
-    // private static final ClassMap<ClavaNode, ClavaNode> CHILDREN_PROCESSORS;
-    //
-    // static {
-    // CHILDREN_PROCESSORS = new ClassMap<>();
-    // }
+    private static final FunctionClassMap<ClavaNode, ClavaNode> CHILDREN_PROCESSORS;
+
+    static {
+        CHILDREN_PROCESSORS = new FunctionClassMap<ClavaNode, ClavaNode>();
+
+        // Process first methods, and functions only after methods
+        CHILDREN_PROCESSORS.put(CXXMethodDecl.class, ClavaAstMethods::processChild);
+        CHILDREN_PROCESSORS.put(FunctionDecl.class, ClavaAstMethods::processChild);
+        CHILDREN_PROCESSORS.put(CXXRecordDecl.class, ClavaAstMethods::processChild);
+        CHILDREN_PROCESSORS.setDefaultFunction(child -> child);
+    }
 
     public ClavaAstMethods(WeaverEngine engine, Class<ClavaNode> nodeClass,
             Function<ClavaNode, JoinPoint> toJoinPointFunction, Function<ClavaNode, String> toJoinPointNameFunction,
             Function<ClavaNode, List<ClavaNode>> scopeChildrenGetter) {
+
         super(engine, nodeClass, toJoinPointFunction, toJoinPointNameFunction, scopeChildrenGetter);
-        // TODO Auto-generated constructor stub
     }
 
     @Override
@@ -206,64 +214,129 @@ public class ClavaAstMethods extends TreeNodeAstMethods<ClavaNode> {
     // return children;
     // }
 
-    protected static ClavaNode processChild(ClavaNode child) {
+    private static ClavaNode processChild(CXXMethodDecl method) {
 
-        // When class method, only return if part of the class, and transform into canonical form
-        // This way, we avoid returning duplicated methods, as they usually appear in C++,
-        // once as a declaration inside the class, and again as an implementation in another file
-        // with the possibility of appearing more than one declaration
-        if (child instanceof CXXMethodDecl) {
-            var method = (CXXMethodDecl) child;
+        var record = method.getRecordDecl().orElse(null);
 
-            var record = method.getRecordDecl().orElse(null);
-
-            // There is no RecordDecl
-            // TODO: return it if canonical?
-            if (record == null) {
-                if (method.isCanonical()) {
-                    return method;
-                } else {
-                    return null;
-                }
-            }
-
-            var parentRecord = method.getAncestorTry(RecordDecl.class).orElse(null);
-
-            // Method has a record and is not inside the class, ignore
-            if (parentRecord == null) {
+        // There is no RecordDecl
+        // TODO: return it if canonical?
+        if (record == null) {
+            if (method.isCanonical()) {
+                return method;
+            } else {
                 return null;
             }
-
-            // Method has a class ancestor, but is not its own class
-            // TODO: Ignore it?
-            if (!parentRecord.equals(record)) {
-                return null;
-            }
-
-            // Return the canonical version
-            return method.canonical();
-            // return ((CXXMethodDecl) child).canonical();
         }
 
-        if (child instanceof FunctionDecl) {
-            var function = (FunctionDecl) child;
+        var parentRecord = method.getAncestorTry(RecordDecl.class).orElse(null);
 
-            // If definition, is always valid
-            if (function.isDefinition()) {
-                return function;
-            }
-
-            // It is a declaration, only valid if there is no definition
-            // AND if it is the first declaration as returned by .getDefinition()
-            if (function.getDefinition().isEmpty() && function.getDeclaration().get().equals(function)) {
-                return function;
-            }
-
-            // In any other case, ignore the node
+        // Method has a record and is not inside the class, ignore
+        if (parentRecord == null) {
             return null;
         }
 
-        return child;
+        // Method has a class ancestor, but is not its own class
+        // TODO: Ignore it?
+        if (!parentRecord.equals(record)) {
+            return null;
+        }
+
+        // Return the canonical version
+        return method.canonical();
+
+    }
+
+    private static ClavaNode processChild(FunctionDecl function) {
+        // If definition, is always valid
+        if (function.isDefinition()) {
+            return function;
+        }
+
+        // It is a declaration, only valid if there is no definition
+        // AND if it is the first declaration as returned by .getDefinition()
+        if (function.getDefinition().isEmpty() && function.getDeclaration().get().equals(function)) {
+            return function;
+        }
+
+        // In any other case, ignore the node
+        return null;
+    }
+
+    private static ClavaNode processChild(CXXRecordDecl cxxRecordDecl) {
+
+        // If definition, is always valid
+        if (cxxRecordDecl.isCompleteDefinition()) {
+            return cxxRecordDecl;
+        }
+
+        // It is a declaration, only valid if there is no definition
+        // AND if it is the first declaration as returned by .getDefinition()
+        if (cxxRecordDecl.getDefinition().isEmpty() && cxxRecordDecl.getDeclaration().get().equals(cxxRecordDecl)) {
+            return cxxRecordDecl;
+        }
+
+        // In any other case, ignore the node
+        return null;
+    }
+
+    protected static ClavaNode processChild(ClavaNode child) {
+        return CHILDREN_PROCESSORS.apply(child);
+        // // When class method, only return if part of the class, and transform into canonical form
+        // // This way, we avoid returning duplicated methods, as they usually appear in C++,
+        // // once as a declaration inside the class, and again as an implementation in another file
+        // // with the possibility of appearing more than one declaration
+        // if (child instanceof CXXMethodDecl) {
+        // var method = (CXXMethodDecl) child;
+        //
+        // var record = method.getRecordDecl().orElse(null);
+        //
+        // // There is no RecordDecl
+        // // TODO: return it if canonical?
+        // if (record == null) {
+        // if (method.isCanonical()) {
+        // return method;
+        // } else {
+        // return null;
+        // }
+        // }
+        //
+        // var parentRecord = method.getAncestorTry(RecordDecl.class).orElse(null);
+        //
+        // // Method has a record and is not inside the class, ignore
+        // if (parentRecord == null) {
+        // return null;
+        // }
+        //
+        // // Method has a class ancestor, but is not its own class
+        // // TODO: Ignore it?
+        // if (!parentRecord.equals(record)) {
+        // return null;
+        // }
+        //
+        // // Return the canonical version
+        // return method.canonical();
+        // // return ((CXXMethodDecl) child).canonical();
+        // }
+        //
+        // if (child instanceof FunctionDecl) {
+        // var function = (FunctionDecl) child;
+        //
+        // // If definition, is always valid
+        // if (function.isDefinition()) {
+        // return function;
+        // }
+        //
+        // // It is a declaration, only valid if there is no definition
+        // // AND if it is the first declaration as returned by .getDefinition()
+        // if (function.getDefinition().isEmpty() && function.getDeclaration().get().equals(function)) {
+        // return function;
+        // }
+        //
+        // // In any other case, ignore the node
+        // return null;
+        // }
+        //
+        // return child;
     }
 
 }
