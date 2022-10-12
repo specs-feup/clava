8f4b1b85c0d233fe56a882113e66471e8a7d7b3c
Joao Bispo <jbispo@fe.up.pt>
2022-10-12 12:20:48 +0100

    [ClavaWeaver] Disables syntax check of C++ API test MpiScatterGather
    - If MPI is not installed it will fail with syntax check on, since it
    will not find mpi.h;

--- ClavaWeaver/test/pt/up/fe/specs/cxxweaver/tests/CxxApiTest.java
+++ ClavaWeaver/test/pt/up/fe/specs/cxxweaver/tests/CxxApiTest.java
@@ -200,7 +200,9 @@ public class CxxApiTest {
 
     @Test
     public void testMpiScatterGather() {
-        newTester()
-                .test("MpiScatterGatherTest.js", "mpi_scatter_gather.cpp", "mpi_scatter_gather.h");
+        newTester()    
+        // Disable syntax check of woven code, mpi.h may not be available
+        .setCheckWovenCodeSyntax(false)
+        	.test("MpiScatterGatherTest.js", "mpi_scatter_gather.cpp", "mpi_scatter_gather.h");
     }
 }
