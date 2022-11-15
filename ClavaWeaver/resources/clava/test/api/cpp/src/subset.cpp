3c2af588cdf3b848a6573a671fff9e18c544da21
João Bispo <joaobispo@gmail.com>
2022-11-15 11:57:30 +0000

    [ClavaWeaver] Improves C++ API test 'Subset'

--- ClavaWeaver/resources/clava/test/api/cpp/src/subset.cpp
+++ ClavaWeaver/resources/clava/test/api/cpp/src/subset.cpp
@@ -1,7 +1,17 @@
 #include <string.h>
+#include <fstream>
 
 void strcmpTest() {
 	if (strcmp("a", "b") == 0) {
 		return;
 	}
+}
+
+void fstreamTest(const char *fn) {
+  std::fstream f(fn, std::fstream::in);
+
+  while (f.good())
+  {
+    return;
+  }	
 }
