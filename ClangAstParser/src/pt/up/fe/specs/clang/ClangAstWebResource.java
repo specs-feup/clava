847b87ca575069a82596f936b2870d3568c162d9
João Bispo <joaobispo@gmail.com>
2023-02-19 03:19:31 +0000

    [ClangAstParser] Updates builtins web resources

--- ClangAstParser/src/pt/up/fe/specs/clang/ClangAstWebResource.java
+++ ClangAstParser/src/pt/up/fe/specs/clang/ClangAstWebResource.java
@@ -25,7 +25,7 @@ public interface ClangAstWebResource {
         return WebResourceProvider.newInstance("http://specs.fe.up.pt/resources/clangast/", resourceUrl);
     }
 
-    WebResourceProvider BUILTIN_INCLUDES = create("clang_builtin_includes_v14.0.6.zip", "v14.0.6");
+    WebResourceProvider BUILTIN_INCLUDES = create("clang_builtin_includes_v14.0.6.zip_", "v14.0.6_2");
     // WebResourceProvider LIBC_CXX = create("libcxx_7.0.1.zip", "v1.0");
     WebResourceProvider LIBC_CXX = create("libc_cxx.zip", "v14.0.6");
 
