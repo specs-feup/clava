3159d161d85b8bea874c491d20cd0a674907b23a
L. Sousa <lm.sousa@fe.up.pt>
2023-07-01 23:17:22 +0100

    Refactor weaver configuration to allow for usage during testing

--- src-code/index.ts
+++ src-code/index.ts
@@ -1,14 +1,6 @@
 #!/usr/bin/env node
 import WeaverLauncher from "lara-js/code/WeaverLauncher.js";
-import WeaverConfiguration from "lara-js/code/WeaverConfiguration.js";
-
-const weaverConfig: WeaverConfiguration = {
-  weaverName: "clava",
-  weaverPrettyName: "Clava",
-  weaverFileName: "Weaver.js",
-  jarFilePath: "../../../SPeCS/ClavaWeaver.jar",
-  javaWeaverQualifiedName: "pt.up.fe.specs.clava.weaver.CxxWeaver",
-};
+import weaverConfig from "./WeaverConfiguration.js";
 
 const weaverLauncher = new WeaverLauncher(weaverConfig);
 
