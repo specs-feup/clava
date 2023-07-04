dc50cc6965ddfdbee28a2aa0de628e1f38cc3b8a
L. Sousa <lm.sousa@fe.up.pt>
2023-07-04 11:34:18 +0100

    Fix type error

--- src-code/WeaverConfiguration.ts
+++ src-code/WeaverConfiguration.ts
@@ -1,11 +1,9 @@
 import WeaverConfiguration from "lara-js/code/WeaverConfiguration.js";
 
-const weaverConfig: WeaverConfiguration = {
+export const weaverConfig: WeaverConfiguration = {
   weaverName: "clava",
   weaverPrettyName: "Clava",
   weaverFileName: "Weaver.js",
   jarFilePath: "../../../SPeCS/ClavaWeaver.jar",
   javaWeaverQualifiedName: "pt.up.fe.specs.clava.weaver.CxxWeaver",
 };
-
-export default weaverConfig;
--- src-code/index.ts
+++ src-code/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import WeaverLauncher from "lara-js/code/WeaverLauncher.js";
-import weaverConfig from "./WeaverConfiguration.js";
+import { weaverConfig } from "./WeaverConfiguration.js";
 
 const weaverLauncher = new WeaverLauncher(weaverConfig);
 
