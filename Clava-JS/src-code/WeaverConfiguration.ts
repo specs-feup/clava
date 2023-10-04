4fe732d38910cd211202c3804663fde6e531a27b
L. Sousa <lm.sousa@fe.up.pt>
2023-10-04 20:51:14 +0100

    [Clava-JS] Bug fix with path of jar files

--- Clava-JS/src-code/WeaverConfiguration.ts
+++ Clava-JS/src-code/WeaverConfiguration.ts
@@ -1,9 +1,16 @@
 import WeaverConfiguration from "lara-js/code/WeaverConfiguration.js";
+import path from "path";
+import { fileURLToPath, pathToFileURL } from "url";
 
 export const weaverConfig: WeaverConfiguration = {
   weaverName: "clava",
   weaverPrettyName: "Clava",
   weaverFileName: "Weaver.js",
-  jarFilePath: "../../ClavaWeaver.jar",
+  jarFilePath: pathToFileURL(
+    path.join(
+      path.dirname(path.dirname(fileURLToPath(import.meta.url))),
+      "./java-binaries/ClavaWeaver.jar"
+    )
+  ).toString(),
   javaWeaverQualifiedName: "pt.up.fe.specs.clava.weaver.CxxWeaver",
 };
