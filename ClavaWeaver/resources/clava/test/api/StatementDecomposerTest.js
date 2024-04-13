e7ab501961d6365afd3d4cfe48966785e2dddfa9
João Bispo <jbispo@fe.up.pt>
2024-04-13 20:32:35 +0100

    [ClavaWeaver] Adds missing files from previous commit

--- ClavaWeaver/resources/clava/test/api/StatementDecomposerTest.js
+++ ClavaWeaver/resources/clava/test/api/StatementDecomposerTest.js
@@ -6,16 +6,6 @@ var decomposer = new StatementDecomposer();
 
 for (var $stmt of Query.search("function", "foo").search("statement")) {
   decomposer.decomposeAndReplace($stmt);
-  /*
-		var decomposedStmts = decomposer.decompose($stmt);
-		
-		if(decomposedStmts.length > 0) {
-			println("Original stmt:" + $stmt.code);
-			for(var $decompStmt of decomposedStmts) {
-				println("Decomp: " + $decompStmt.code);
-			}		
-		}
-		*/
 }
 
 println(Query.search("function", "foo").first().code);
--- ClavaWeaver/resources/clava/test/api/cpp/src/stmt_decomposer.cpp
+++ ClavaWeaver/resources/clava/test/api/cpp/src/stmt_decomposer.cpp
@@ -5,4 +5,3 @@ int foo() {
 	
 	return a + c;
 }
-
