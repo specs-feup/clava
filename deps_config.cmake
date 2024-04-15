b9c2d4e274d7f7ba3b492451190e553687a224f2
L. Sousa <lm.sousa@fe.up.pt>
2024-04-15 14:27:54 +0100

    Partial revert of commit c62c1d8c62b0c97f042f8e62209fbce1ae0b78cc

---
+++ deps_config.cmake
@@ -0,0 +1,14 @@
+#
+# Configurations to be used with Deps
+#
+cmake_minimum_required(VERSION 3.2)
+
+#set(HOST_SPECS_TOOLS "specs-tools")
+
+# SYSTEM_PLATFORM: Manually set the platform, in case it could not be automatically detected
+# Supported values: Centos, MacOS
+#set(SYSTEM_PLATFORM "Centos")
+#set(SYSTEM_PLATFORM "MacOS")
+
+# DEPS_COMPILER: Manually set the compiler
+set(DEPS_COMPILER "gcc5")
