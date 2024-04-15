44508bf2660fa00a7adc6983828bed676623b7f9
L. Sousa <lm.sousa@fe.up.pt>
2024-04-15 14:28:40 +0100

    Partial revert of commit c62c1d8c62b0c97f042f8e62209fbce1ae0b78cc. Reintroduction of the ClavaLaraExtraApi folder

---
+++ ClavaLaraExtraApi/src-lara/clava/ClavaMathExtra.js
@@ -0,0 +1,19 @@
+class ClavaMathExtra {
+	
+	/**
+	 * Attempts to simplify a C/C++ expression, returning an $expression join point with 
+	 * the result of the simplification, or undefined if it was not possible to simplify
+	 * the expression.
+	 *
+	 * @param {$expression} $expression - The C/C++ expression to simplify.
+	 * @param {object} constants - An object that maps variable names to constants.
+	 *
+	 * @returns {$expression} the simplified expression, or undefined if it was not possible 
+	 * to simplify the expression
+	 *
+	 */
+	static simplify($expression, constants) {
+		throw new Error("Not yet implemented");
+	}
+	
+}
