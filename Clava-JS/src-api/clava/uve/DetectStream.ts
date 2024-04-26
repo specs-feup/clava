9159147a75e1a513d6c46f9b4f407eac59a8bc76
L. Sousa <lm.sousa@fe.up.pt>
2024-04-26 14:25:26 +0100

    Fix #139. Remove useless APIs

--- Clava-JS/src-api/clava/uve/DetectStream.ts
+++
@@ -1,5 +0,0 @@
-import { FunctionJp, Param } from "../../Joinpoints.js";
-
-export default class DetectStream {
-  static detectLinear(func: FunctionJp, arr: Param) {}
-}
--- ClavaLaraApi/src-lara/clava/clava/uve/DetectStream.js
+++
@@ -1,4 +0,0 @@
-export default class DetectStream {
-    static detectLinear(func, arr) { }
-}
-//# sourceMappingURL=DetectStream.js.map
