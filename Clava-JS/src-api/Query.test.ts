3c510da02ca8287ddb5b0e55d0df21fd693674c0
L. Sousa <lm.sousa@fe.up.pt>
2024-05-14 17:19:41 +0100

    Simplify test code using new helper script from Lara-JS

--- Clava-JS/src-api/Query.test.ts
+++ Clava-JS/src-api/Query.test.ts
@@ -1,54 +1,36 @@
-import Clava from "clava-js/api/clava/Clava.js";
-import ClavaJoinPoints from "clava-js/api/clava/ClavaJoinPoints.js";
+import { registerSourceCode } from "lara-js/jest/jestHelpers.js";
 import Query from "lara-js/api/weaver/Query.js";
 import { FunctionJp, Joinpoint, Loop } from "./Joinpoints.js";
 
-describe("Query", () => {
-  beforeAll(() => {
-    const code = `void query_loop() {
-	
-	for(int i=0; i<10; i++) {
-		
-	}
-	
-	for(int j1=0; j1<10; j1++) {
-		for(int j2=0; j2<10; j2++) {
-		}		
-	}		
+const code = `void query_loop() {
+    for(int i=0; i<10; i++) {
+    }
 
-	for(int k1=0; k1<10; k1++) {
-		for(int k2=0; k2<10; k2++) {
-			for(int k3=0; k3<10; k3++) {
-			}		
-		}		
-	}		
-	
+    for(int j1=0; j1<10; j1++) {
+        for(int j2=0; j2<10; j2++) {
+        }
+    }
+
+    for(int k1=0; k1<10; k1++) {
+        for(int k2=0; k2<10; k2++) {
+            for(int k3=0; k3<10; k3++) {
+            }
+        }
+    }
 }
-
 
 int query_empty() {
-	
-	int a;
-	return a + 2;
-	
+    int a;
+    return a + 2;
 }
 
-
-int query_regex() {	
-	return 0;
+int query_regex() {
+    return 0;
 }
 `;
 
-    Clava.getProgram().push();
-    const program = Clava.getProgram();
-    const sourceFile = ClavaJoinPoints.fileWithSource("dummyFile.cpp", code);
-    program.addFile(sourceFile);
-    program.rebuild();
-  });
-
-  afterAll(() => {
-    Clava.getProgram().pop();
-  });
+describe("Query", () => {
+  registerSourceCode(code);
 
   it("should be able to search for a function", () => {
     const lst: number[][] = [];
