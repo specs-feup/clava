7ec1d6f342445e8364abdbae8510387a928e6efe
racoelhosilva <racoelhosilva@gmail.com>
2024-07-19 17:36:54 +0100

    feat: added InsertBefore and InsertAfter events

--- Clava-JS/src-api/clava/history/EventListener.ts
+++ Clava-JS/src-api/clava/history/EventListener.ts
@@ -1,7 +1,9 @@
 import { EventEmitter } from "events";
 import * as fs from "fs";
 import Clava from "../Clava.js";
-import { Event } from "./Events.js";
+import { Event, EventTime } from "./Events.js";
+import ophistory from "./History.js";
+import { InsertOperation } from "./Operations.js";
 
 const eventListener = new EventEmitter();
 
@@ -12,11 +14,51 @@ eventListener.on("storeAST", () => {
   fs.writeFileSync(`history/waypoint_${idx}.cpp`, Clava.getProgram().code);
   idx++;
 });
+
 eventListener.on("ACTION", (e: Event) => {
+  
+  // Event Logs for debugging
+
+  console.log("\nReceived ACTION event");
   console.log(e.timing);
   console.log(e.description);
   console.log(e.mainJP);
   console.log(e.returnJP);
+  console.log(e.inputs);
+  console.log("\n");
+
+  switch (e.timing) {
+    case EventTime.BEFORE:
+      break;
+
+    case EventTime.AFTER:
+      switch (e.description){
+        case "insertAfter":
+        case "insertBefore":
+          if (e.returnJP !== undefined){
+            ophistory.newOperation(new InsertOperation(e.returnJP))
+          }
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+
+  // Manual testing the rollback
+  /*
+  console.log(`Waypoint ${idx}`);
+  fs.writeFileSync(`history/waypoint_${idx}.cpp`, Clava.getProgram().code);
+  idx++;
+
+  ophistory.rollback();
+
+  console.log(`Waypoint ${idx}`);
+  fs.writeFileSync(`history/waypoint_${idx}.cpp`, Clava.getProgram().code);
+  idx++;
+  */
 });
 
 export default eventListener;
--- Clava-JS/src-api/clava/history/Operations.ts
+++ Clava-JS/src-api/clava/history/Operations.ts
@@ -1,4 +1,13 @@
+import { Joinpoint } from "../../Joinpoints.js";
 
 export interface Operation {
-    undo(): void;
+  undo(): void;
+}
+
+export class InsertOperation implements Operation {
+  constructor(private newJP: Joinpoint) {}
+
+  undo(): void {
+      this.newJP.detach();
+  }
 }
