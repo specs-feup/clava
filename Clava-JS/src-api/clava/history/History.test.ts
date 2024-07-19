c108726409e33ca9c6e23c673acc027d1d15b07a
racoelhosilva <racoelhosilva@gmail.com>
2024-07-19 18:27:32 +0100

    test: replaceWith with singular string and JoinPoint, replaceWithStrings and toComment behaviour

--- Clava-JS/src-api/clava/history/History.test.ts
+++ Clava-JS/src-api/clava/history/History.test.ts
@@ -2,7 +2,7 @@ import { registerSourceCode } from "lara-js/jest/jestHelpers";
 import Clava from "../../../api/clava/Clava";
 import Query from "lara-js/api/weaver/Query";
 import ophistory from "./History";
-import { Loop } from "../../Joinpoints";
+import { Loop, ReturnStmt } from "../../Joinpoints";
 
 const code: string = `void func() {
     for (int i = 0; i < 1; i++){
@@ -49,6 +49,88 @@ describe("History of Transformations", () => {
         ophistory.rollback();
         const c: string = Clava.getProgram().code;
   
+        expect(a).toEqual(c);
+        expect(b).not.toEqual(c);
+    });
+
+    it("Initial code, replace singular string and rollback code comparison", () => {
+        const a: string = Clava.getProgram().code;
+    
+        const loopStmt = Query.search(Loop).get().at(0);
+        loopStmt?.replaceWith("aaaaa"); 
+    
+        const b: string = Clava.getProgram().code;
+        
+        ophistory.rollback();
+        const c: string = Clava.getProgram().code;
+        
+        expect(a).toEqual(c);
+        expect(b).not.toEqual(c);
+    });
+
+    it("Initial code, replace singular joinpoint and rollback code comparison", () => {
+        const a: string = Clava.getProgram().code;
+    
+        const loopStmt = Query.search(Loop).get().at(0);
+        const returnStmt = Query.search(ReturnStmt).first();
+        if (returnStmt !== undefined){
+            loopStmt?.replaceWith(returnStmt.deepCopy()); 
+        }
+    
+        const b: string = Clava.getProgram().code;
+        
+        ophistory.rollback();
+        const c: string = Clava.getProgram().code;
+        
+        expect(a).toEqual(c);
+        expect(b).not.toEqual(c);
+    });
+
+    it("Initial code, replace multiple joinpoints and rollback code comparison", () => {
+        // TODO: fix error regarding Joinpoint[] overload not found
+        /*const a: string = Clava.getProgram().code;
+    
+        const loopStmt = Query.search(Loop).get().at(0);
+        const returnStmt = Query.search(ReturnStmt).first();
+        if (returnStmt !== undefined){
+            loopStmt?.replaceWith(returnStmt.deepCopy()); 
+        }
+    
+        const b: string = Clava.getProgram().code;
+        
+        ophistory.rollback();
+        const c: string = Clava.getProgram().code;
+        
+        expect(a).toEqual(c);
+        expect(b).not.toEqual(c);*/
+    });
+
+    it("Initial code, replace multiple strings and rollback code comparison", () => {
+        const a: string = Clava.getProgram().code;
+    
+        const loopStmt = Query.search(Loop).get().at(0);
+        loopStmt?.replaceWithStrings(["aaaa", "bbbb", "cccc"]);
+    
+        const b: string = Clava.getProgram().code;
+        
+        ophistory.rollback();
+        const c: string = Clava.getProgram().code;
+        
+        expect(a).toEqual(c);
+        expect(b).not.toEqual(c);
+    });
+
+    it("Initial code, comment joinpoint and rollback code comparison", () => {
+        const a: string = Clava.getProgram().code;
+    
+        const loopStmt = Query.search(Loop).get().at(0);
+        loopStmt?.toComment();
+    
+        const b: string = Clava.getProgram().code;
+        
+        ophistory.rollback();
+        const c: string = Clava.getProgram().code;
+        
         expect(a).toEqual(c);
         expect(b).not.toEqual(c);
     });
