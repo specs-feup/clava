ea8cfc0de7f114e0022fa7be42c272d6a4c56ba5
Process-ing <rbruno.p.o@gmail.com>
2024-07-27 01:45:38 +0100

    Write documentation for ClavaAstConverter.ts

--- ClavaLaraApi/src-lara/clava/visualization/ClavaAstConverter.js
+++ ClavaLaraApi/src-lara/clava/visualization/ClavaAstConverter.js
@@ -2,10 +2,20 @@ import ToolJoinPoint from "lara-js/api/visualization/public/js/ToolJoinPoint.js"
 import { AccessSpecifier, AdjustedType, Body, BoolLiteral, Break, Call, Case, Class, Comment, Continue, Decl, Declarator, DeclStmt, EnumDecl, EnumeratorDecl, ExprStmt, FileJp, FloatLiteral, FunctionJp, GotoStmt, If, Include, IntLiteral, Literal, Loop, NamedDecl, Omp, Pragma, Program, RecordJp, ReturnStmt, Scope, Statement, Switch, Tag, Type, TypedefDecl, Vardecl, Varref, WrapperStmt } from "../Joinpoints.js";
 import Clava from "../clava/Clava.js";
 import { addIdentation, escapeHtml, getNodeCodeTags, getSyntaxHighlightTags } from "lara-js/api/visualization/AstConverterUtils.js";
+/**
+ * @brief Clava specialization of GenericAstConverter.
+ */
 export default class ClavaAstConverter {
     updateAst() {
         Clava.rebuild();
     }
+    /**
+     * @brief Returns the code of the given node.
+     * @details If the node has no code, returns undefined, instead of raising an error.
+     *
+     * @param node Node
+     * @returns The code of the node, or undefined if it has no code
+     */
     getCode(node) {
         // TODO: When hasCode implementation is ready, replace this body with the following line:
         // return node.hasCode ? node.code.trim() : undefined
@@ -19,6 +29,12 @@ export default class ClavaAstConverter {
         }
         return code;
     }
+    /**
+     * @brief Returns the information of the given join point, according to its type.
+     *
+     * @param jp Join point
+     * @returns The information of the join point
+     */
     getJoinPointInfo(jp) {
         const info = {
             'AST ID': jp.astId,
@@ -65,6 +81,7 @@ export default class ClavaAstConverter {
                 'Constant?': jp.constant,
                 'Is builtin': jp.isBuiltin,
                 'Has sugar': jp.hasSugar,
+                'Type kind': jp.kind
             });
         }
         if (jp instanceof Varref) {
@@ -126,6 +143,14 @@ export default class ClavaAstConverter {
         };
         return toToolJoinPoint(root);
     }
+    /**
+     * @brief Converts the given join point to a CodeNode.
+     * @details This function assigns ids to the nodes in pre-order, and it MUST
+     * be the same as the order used for getToolAst.
+     *
+     * @param jp Join point
+     * @returns The CodeNode representation of the join point
+     */
     toCodeNode(jp) {
         let nextId = 0;
         const toCodeNode = (jp) => {
@@ -138,6 +163,15 @@ export default class ClavaAstConverter {
         };
         return toCodeNode(jp);
     }
+    /**
+     * @brief Sorts the given code nodes by location.
+     * @details This function uses the join points' line and column, instead of
+     * location attribute. If a node does not have a location (line and code),
+     * it is placed before all the others.
+     *
+     * @param codeNodes Array of code nodes
+     * @returns Reference to the original (sorted) array
+     */
     sortByLocation(codeNodes) {
         return codeNodes.sort((node1, node2) => {
             if (node1.jp.line === node2.jp.line)
@@ -145,6 +179,17 @@ export default class ClavaAstConverter {
             return (node1.jp.line ?? -1) - (node2.jp.line ?? -1);
         });
     }
+    /**
+     * @brief Applies refinements and corrections to the node code and order of
+     * the children.
+     * @details This is necessary because some nodes do not have its designated
+     * code equal to the matching code in the container. For example, the code
+     * inside code blocks do not have its indentation.
+     *
+     * @param node The node code
+     * @param indentation The current indentation to use
+     * @returns Reference to the original (refined) node
+     */
     refineCode(node, indentation = 0) {
         if (node.code)
             node.code = addIdentation(node.code, indentation);
@@ -158,8 +203,8 @@ export default class ClavaAstConverter {
             node.children
                 .slice(1)
                 .forEach(child => {
-                child.code = child.code.match(/^(?:\S+\s+)(\S.*)$/)[1];
-            }); // Remove type from variable declarations
+                child.code = child.code.match(/^(?:\S+\s+)(\S.*)$/)[1]; // Remove type from variable declarations
+            });
         }
         for (const child of node.children) {
             const newIndentation = (node.jp instanceof Scope || node.jp instanceof Class) ? indentation + 1 : indentation;
@@ -169,9 +214,9 @@ export default class ClavaAstConverter {
             const match = node.code.match(/^([^\/]*\S)\s*(\/\/.*)$/);
             if (match) {
                 const [, statement, comment] = match;
-                node.code = statement + '  ' + comment;
+                node.code = statement + '  ' + comment; // Fix space between statement and inline comment in naked body
             }
-        } // Fix space between statement and inline comment in naked body
+        }
         if (node.children.length >= 1 && node.children[0].jp.astName === 'TagDeclVars') {
             const tagDeclVars = node.children[0];
             const typedef = tagDeclVars.children[0];
@@ -197,6 +242,16 @@ export default class ClavaAstConverter {
         } // Divide program code into its files
         return node;
     }
+    /**
+     * @brief Performs C/C++ syntax hioverlighting on the given code.
+     * @details This is done by inserting the appropriate span tags around the
+     * code to be highlighted, using the HTML code as a string
+     *
+     * @param code The code to be highlighted, with the code of its children
+     * already linked and syntax highlighted
+     * @param node The node that the code belongs to
+     * @returns The syntax highlighted code
+     */
     syntaxHighlight(code, node) {
         if (code === undefined)
             return undefined;
@@ -211,11 +266,12 @@ export default class ClavaAstConverter {
         const [openingTag, closingTag] = getSyntaxHighlightTags('comment');
         if (node.jp instanceof Comment)
             return openingTag + code + closingTag;
-        code = code.replaceAll(/(?<!>)(\/\/.*)/g, `${openingTag}$1${closingTag}`);
-        code = code.replaceAll(/(?<!>)(\/\*.*?\*\/)/g, `${openingTag}$1${closingTag}`);
+        code = code.replaceAll(/(?<!>)(\/\/.*)/g, `${openingTag}$1${closingTag}`); // Highlight unhighlighted single-line comments
+        code = code.replaceAll(/(?<!>)(\/\*.*?\*\/)/g, `${openingTag}$1${closingTag}`); // Highlight unhighlighted multi-line comments
+        // We know that a piece of code is not highlighted if it is not preceded by a '>' (from the opening span tag)
         if (node.jp instanceof Declarator || node.jp instanceof EnumeratorDecl) {
             const [openingTag, closingTag] = getSyntaxHighlightTags('type');
-            const regex = new RegExp(`\\s*[&*]*\\b${node.jp.name}\\b`);
+            const regex = new RegExp(`\\s*[&*]*\\b${node.jp.name}\\b`); // Match the declaration name with the '&' and '*' prefixes
             const namePos = code.search(regex);
             return namePos <= 0 ? code : openingTag + code.slice(0, namePos) + closingTag + code.slice(namePos);
         }
@@ -228,7 +284,8 @@ export default class ClavaAstConverter {
                 return code.replace(/^(\w+)\b/, `${openingTag}$1${closingTag}`); // Highlight first word
             }
             if (node.jp instanceof If) {
-                const elseMatch = code.match(/^(([^/]|\/[^/*]|\/\/.*|\/\*([^*]|\*[^/])*\*\/)*?)(?<!>)\belse\b/);
+                const elseMatch = code.match(/^(([^/]|\/[^/*]|\/\/.*|\/\*([^*]|\*[^/])*\*\/)*?)(?<!>)\belse\b/); // Match first unhighlighted 'else' that is not inside a comment (single or multi-line)
+                // Note that the function is meant to be called recursively, so the 'else' of child ifs are already highlighted
                 if (elseMatch) {
                     const elsePos = elseMatch[1].length;
                     return openingTag + 'if' + closingTag + code.slice(2, elsePos) + openingTag + 'else' + closingTag + code.slice(elsePos + 4);
@@ -239,7 +296,8 @@ export default class ClavaAstConverter {
             }
             if (node.jp instanceof Loop) {
                 if (node.jp.kind == 'dowhile') {
-                    const whilePos = code.match(/^(([^/]|\/[^/*]|\/\/.*|\/\*([^*]|\*[^/])*\*\/)*?)(?<!>)\bwhile\b/)[1].length;
+                    const whilePos = code.match(/^(([^/]|\/[^/*]|\/\/.*|\/\*([^*]|\*[^/])*\*\/)*?)(?<!>)\bwhile\b/)[1].length; // Match first unhighlighted 'while' that is not inside a comment (single or multi-line)
+                    // Same logic as the 'else' keyword
                     return openingTag + 'do' + closingTag + code.slice(2, whilePos) + openingTag + 'while' + closingTag + code.slice(whilePos + 5);
                 }
                 else {
@@ -253,21 +311,33 @@ export default class ClavaAstConverter {
                 return code.replace(/(class(?!=)|struct)/, `${openingTag}$1${closingTag}`); // Highlight 'class' or 'struct' in declaration
             }
             if (node.jp instanceof Include || node.jp instanceof Pragma) {
-                return code.replace(/^(#\w+)\b/, `${openingTag}$1${closingTag}`);
+                return code.replace(/^(#\w+)\b/, `${openingTag}$1${closingTag}`); // Highlight the directive
             }
         }
         return code;
     }
+    /**
+     * @brief Links the nodes of the given AST to their respective portion of code.
+     *
+     * @param root The root node of the AST
+     * @param outerCode The outer code, which should contain the code of all the
+     * nodes
+     * @param outerCodeStart The start index of the outer code section to be used
+     * @param outerCodeEnd The end index of the outer code section to be used
+     * @returns Array with three elements: the start index of the node code in the
+     * outer code, the end index of the node code in the outer code, and the
+     * linked code.
+     */
     linkCodeToAstNodes(root, outerCode, outerCodeStart, outerCodeEnd) {
         const nodeCode = root.code;
         if (!nodeCode || !outerCode)
-            return [outerCodeStart, outerCodeStart, ""];
+            return [outerCodeStart, outerCodeStart, ""]; // Return empty string if the node has no code
         const nodeCodeHtml = escapeHtml(nodeCode);
         const innerCodeStart = outerCode.indexOf(nodeCodeHtml, outerCodeStart);
         const innerCodeEnd = innerCodeStart + nodeCodeHtml.length;
         if (innerCodeStart === -1 || innerCodeEnd > outerCodeEnd) {
             console.warn(`Code of node "${root.jp.joinPointType}" not found in code container: "${nodeCodeHtml}"`);
-            return [outerCodeStart, outerCodeStart, ""];
+            return [outerCodeStart, outerCodeStart, ""]; // Return empty string if the node code is not found
         }
         const [openingTag, closingTag] = getNodeCodeTags(root.id);
         let newCode = '';
@@ -282,10 +352,10 @@ export default class ClavaAstConverter {
         } // Ignore function return type and name in declaration
         for (const child of root.children) {
             const [childCodeStart, childCodeEnd, childCode] = this.linkCodeToAstNodes(child, outerCode, newCodeIndex, innerCodeEnd);
-            newCode += outerCode.slice(newCodeIndex, childCodeStart) + childCode;
+            newCode += outerCode.slice(newCodeIndex, childCodeStart) + childCode; // Add portion behind children that is not matched and the linked child code
             newCodeIndex = childCodeEnd;
         }
-        newCode += outerCode.slice(newCodeIndex, innerCodeEnd);
+        newCode += outerCode.slice(newCodeIndex, innerCodeEnd); // Add the remaining unmatched code
         newCode = this.syntaxHighlight(newCode, root);
         return [innerCodeStart, innerCodeEnd, openingTag + newCode + closingTag];
     }
