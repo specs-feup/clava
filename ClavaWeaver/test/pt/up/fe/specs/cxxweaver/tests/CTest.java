095ccc0a3c38d2fa600bbbb0b3a8fc6f036e7143
João Bispo <jbispo@fe.up.pt>
2025-03-14 13:49:26 +0000

    [ClavaWeaver] Fixes $loop.condRelation, adds corresponding test

--- ClavaWeaver/test/pt/up/fe/specs/cxxweaver/tests/CTest.java
+++ ClavaWeaver/test/pt/up/fe/specs/cxxweaver/tests/CTest.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2016 SPeCS.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
@@ -16,7 +16,6 @@ package pt.up.fe.specs.cxxweaver.tests;
 import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
 import pt.up.fe.specs.clava.ClavaOptions;
 import pt.up.fe.specs.clava.language.Standard;
 import pt.up.fe.specs.clava.weaver.options.CxxWeaverOption;
@@ -43,11 +42,6 @@ public class CTest {
                 .setSrcPackage("c/src");
     }
 
-    // TODO: Temporarily disabled, Jenkins fails with "Cannot inherit from final class"
-    // @Test
-    public void testLoop() {
-        newTester().test("Loop.lara", "loop.c");
-    }
 
     @Test
     public void testReplaceCallWithStmt() {
