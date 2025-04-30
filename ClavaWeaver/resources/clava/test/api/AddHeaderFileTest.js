535020b9655f884470049c43254ac41312f169bf
L. Sousa <lm.sousa@fe.up.pt>
2025-04-30 19:37:51 +0100

    Fix test

--- ClavaWeaver/resources/clava/test/api/AddHeaderFileTest.js
+++ ClavaWeaver/resources/clava/test/api/AddHeaderFileTest.js
@@ -1,13 +1,29 @@
 import Clava from "@specs-feup/clava/api/clava/Clava.js";
 import ClavaJoinPoints from "@specs-feup/clava/api/clava/ClavaJoinPoints.js";
 import Io from "@specs-feup/lara/api/lara/Io.js";
+import path from "path";
 
-// Manually add header file
-const headerFile = Io.getPath("add_header_file.h");
+const referenceFolder = Clava.getWeavingFolder();
+const clavaFolder = path.dirname(path.dirname(path.dirname(referenceFolder)));
+const headerFilePath = path.join(
+    clavaFolder,
+    "ClavaWeaver",
+    "resources",
+    "clava",
+    "test",
+    "api",
+    "cpp",
+    "src",
+    "add_header_file.h"
+);
+
+const headerFile = Io.getPath(headerFilePath);
+
 if (!Io.isFile(headerFile)) {
     throw new Error("Could not find header file " + headerFile);
 }
 
+// Manually add header file
 const $file = ClavaJoinPoints.file(headerFile);
 Clava.addFile($file);
 
