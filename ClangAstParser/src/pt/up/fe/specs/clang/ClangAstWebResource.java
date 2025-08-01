5dec1baa7bf7fa83e4ed9624a0cde90300feb771
João Bispo <jbispo@fe.up.pt>
2025-08-01 11:38:04 +0100

    Fix to resource name, Clava-JS v3.0.22

--- ClangAstParser/src/pt/up/fe/specs/clang/ClangAstWebResource.java
+++ ClangAstParser/src/pt/up/fe/specs/clang/ClangAstWebResource.java
@@ -25,7 +25,7 @@ public interface ClangAstWebResource {
         return WebResourceProvider.newInstance("https://specs.fe.up.pt/resources/clangast/", resourceUrl);
     }
 
-    WebResourceProvider BUILTIN_INCLUDES = WebResourceProvider.newInstance("https://github.com/specs-feup/clava/releases/download/clang_ast_dumper_v12.0.7.1/", "clang_builtin_includes.zip", "v12.0.1");
+    WebResourceProvider BUILTIN_INCLUDES = WebResourceProvider.newInstance("https://github.com/specs-feup/clava/releases/download/clang_ast_dumper_v12.0.7.1/", "clang_builtin_includes_v12.0.1.zip", "v12.0.1");
 
     // WebResourceProvider LIBC_CXX = create("libcxx_7.0.1.zip", "v1.0");
     WebResourceProvider LIBC_CXX = create("libc_cxx.zip", "v12.0.1");
