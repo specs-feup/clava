2e1645f917334eed8a07208fa351fe6e3e30be54
João Bispo <jbispo@fe.up.pt>
2025-08-05 23:05:25 +0100

    [ClangAstParser] Increases Linux supported version

--- ClangAstParser/src/pt/up/fe/specs/clang/SupportedPlatform.java
+++ ClangAstParser/src/pt/up/fe/specs/clang/SupportedPlatform.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2017 SPeCS.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
@@ -43,7 +43,7 @@ public enum SupportedPlatform implements StringProvider {
     // private static final int OLDER_LINUX_VERSION = 4;
 
     private static final double SUPPORTED_MAC_VERSION = 11.5;
-    private static final double SUPPORTED_LINUX_VERSION_HIGHER = 5;
+    private static final double SUPPORTED_LINUX_VERSION_HIGHER = 6;
     private static final double SUPPORTED_LINUX_VERSION_LOWER = 4;
 
     public static EnumHelperWithValue<SupportedPlatform> getHelper() {
