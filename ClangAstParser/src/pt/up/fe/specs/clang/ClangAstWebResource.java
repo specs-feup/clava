4896fd1d8700235e154b02ff3fe10fa9b2171b61
João Bispo <jbispo@fe.up.pt>
2025-08-06 12:05:40 +0100

    [ClangAstParser] Fixes web resources URLs

--- ClangAstParser/src/pt/up/fe/specs/clang/ClangAstWebResource.java
+++ ClangAstParser/src/pt/up/fe/specs/clang/ClangAstWebResource.java
@@ -17,38 +17,21 @@ import pt.up.fe.specs.util.providers.WebResourceProvider;
 
 public interface ClangAstWebResource {
 
-    static WebResourceProvider create_old(String resourceUrl, String version) {
-        return WebResourceProvider.newInstance("https://specs.fe.up.pt/resources/clangast/", resourceUrl, version);
-    }
 
-    static WebResourceProvider create_old(String resourceUrl) {
-        return WebResourceProvider.newInstance("https://specs.fe.up.pt/resources/clangast/", resourceUrl);
-    }
+    WebResourceProvider BUILTIN_INCLUDES = WebResourceProvider.newInstance("https://github.com/specs-feup/clava/releases/download/clang_ast_dumper_v12.0.7.1/", "clang_builtin_includes.zip", "v12.0.1");
 
+    WebResourceProvider LIBC_CXX = WebResourceProvider.newInstance("https://github.com/specs-feup/clava/releases/download/clang_ast_dumper_v12.0.7.1/", "libc_cxx.zip", "v12.0.1");
 
-    static WebResourceProvider create(String resourceUrl, String version) {
-        return WebResourceProvider.newInstance("https://github.com/specs-feup/clava/releases/download/", resourceUrl, version);
-    }
+    WebResourceProvider LIBC_CXX_WINDOWS = WebResourceProvider.newInstance("https://github.com/specs-feup/clava/releases/download/clang_ast_dumper_v12.0.7.1/", "libc_cxx_windows.zip", "v12.0.1");
 
-    static WebResourceProvider create(String resourceUrl) {
-        return create(resourceUrl, "1.0");
-    }
+    WebResourceProvider CUDA_LIB = WebResourceProvider.newInstance("https://github.com/specs-feup/clava/releases/download/clang_ast_dumper_v12.0.7.1/", "cudalib.zip", "v11.3.0");
 
-    WebResourceProvider BUILTIN_INCLUDES = create("clang_ast_dumper_v12.0.7.1/clang_builtin_includes_v12.0.1.zip", "v12.0.1");
+    WebResourceProvider WIN_EXE = WebResourceProvider.newInstance("https://github.com/specs-feup/clava/releases/download/clang_ast_dumper_v12.0.7.1/", "clang_ast_windows.exe", "v12.0.7");
+    WebResourceProvider WIN_DLL1 = WebResourceProvider.newInstance("https://github.com/specs-feup/clava/releases/download/clang_ast_dumper_v12.0.7.1/", "libwinpthread-1.dll");
+    WebResourceProvider WIN_DLL2 = WebResourceProvider.newInstance("https://github.com/specs-feup/clava/releases/download/clang_ast_dumper_v12.0.7.1/", "zlib1.dll");
 
-    WebResourceProvider LIBC_CXX = create("clang_ast_dumper_v12.0.7.1/libc_cxx.zip", "v12.0.1");
+    WebResourceProvider LINUX_EXE = WebResourceProvider.newInstance("https://github.com/specs-feup/clava/releases/download/clang_ast_dumper_v12.0.7.1/", "clang_ast_ubuntu", "v12.0.7.1");
 
-    WebResourceProvider LIBC_CXX_WINDOWS = create("clang_ast_dumper_v12.0.7.1/libc_cxx_windows.zip", "v12.0.1");
-
-    // For some reason, on Linux, this file no longer downloads
-    WebResourceProvider CUDA_LIB = create("clang_ast_dumper_v12.0.7.1/cudalib.zip", "v11.3.0");
-
-    WebResourceProvider WIN_EXE = create("clang_ast_dumper_v12.0.7.1/clang_ast_windows.exe", "v12.0.7");
-    WebResourceProvider WIN_DLL1 = create("clang_ast_dumper_v12.0.7.1/libwinpthread-1.dll");
-    WebResourceProvider WIN_DLL2 = create("clang_ast_dumper_v12.0.7.1/zlib1.dll");
-
-    WebResourceProvider LINUX_EXE = create("clang_ast_dumper_v12.0.7.1/clang_ast_ubuntu", "v12.0.7.1");
-
-    WebResourceProvider MAC_OS_EXE = create("clang_ast_dumper_v12.0.7.1/clang_ast_macos", "v12.0.7");
+    WebResourceProvider MAC_OS_EXE = WebResourceProvider.newInstance("https://github.com/specs-feup/clava/releases/download/clang_ast_dumper_v12.0.7.1/", "clang_ast_macos", "v12.0.7");
 
 }
