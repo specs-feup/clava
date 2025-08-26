d3cdc3341d370457001a595469ff2ebc71d585e4
L. Sousa <lm.sousa@fe.up.pt>
2025-08-26 18:41:11 +0100

    Fix import statement and replace println with console.log in IncludeLoc.js

--- ClavaWeaver/resources/clava/test/weaver/IncludeLoc.js
+++ ClavaWeaver/resources/clava/test/weaver/IncludeLoc.js
@@ -1,5 +1,5 @@
-laraImport("weaver.Query")
+import Query from "@specs-feup/lara/api/weaver/Query.js"
 
 for (const include of Query.search("include")) {
-    println(include.code + " -> " + include.line);
+    console.log(include.code + " -> " + include.line);
 }
