2a8344cc2c92a4ec75fd5db245a9b828d4c13183
L. Sousa <lm.sousa@fe.up.pt>
2025-11-04 18:29:26 +0000

    Fix the copyright statement in all affected files.

--- ClavaLaraApi/src-java/pt/up/fe/specs/clava/weaver/memoi/MemoiReportsMap.java
+++ ClavaLaraApi/src-java/pt/up/fe/specs/clava/weaver/memoi/MemoiReportsMap.java
@@ -20,7 +20,7 @@ import pt.up.fe.specs.util.SpecsIo;
  * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License. under the License.
+ * specific language governing permissions and limitations under the License.
  */
 
 public class MemoiReportsMap {
