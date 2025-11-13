1651e14485639edf1aed13e0af692295e652100c
João Bispo <jbispo@fe.up.pt>
2025-11-13 22:15:47 +0000

    [ClavaWeaver] Adapts join point unaryOp to have a standard hierarchy scheme

--- ClavaWeaver/src/pt/up/fe/specs/clava/weaver/joinpoints/CxxUnaryOp.java
+++ ClavaWeaver/src/pt/up/fe/specs/clava/weaver/joinpoints/CxxUnaryOp.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2017 SPeCS.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License. under the License.
@@ -13,10 +13,6 @@
 
 package pt.up.fe.specs.clava.weaver.joinpoints;
 
-import java.util.Arrays;
-import java.util.List;
-
-import pt.up.fe.specs.clava.ClavaNode;
 import pt.up.fe.specs.clava.ClavaNodes;
 import pt.up.fe.specs.clava.ast.expr.UnaryOperator;
 import pt.up.fe.specs.clava.ast.expr.enums.UnaryOperatorKind;
@@ -24,28 +20,31 @@ import pt.up.fe.specs.clava.weaver.CxxJoinpoints;
 import pt.up.fe.specs.clava.weaver.abstracts.joinpoints.AExpression;
 import pt.up.fe.specs.clava.weaver.abstracts.joinpoints.AUnaryOp;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CxxUnaryOp extends AUnaryOp {
 
-    private final UnaryOperator unaryOp;
+//    private final UnaryOperator unaryOp;
 
     public CxxUnaryOp(UnaryOperator unaryOp) {
-        super(new CxxOp(unaryOp));
-        this.unaryOp = unaryOp;
+        super(unaryOp);
+//        this.unaryOp = unaryOp;
     }
 
     @Override
-    public ClavaNode getNode() {
-        return unaryOp;
+    public UnaryOperator getNode() {
+        return (UnaryOperator) super.getNode();
     }
 
     @Override
     public AExpression getOperandImpl() {
-        return CxxJoinpoints.create(ClavaNodes.normalize(unaryOp.getSubExpr()), AExpression.class);
+        return CxxJoinpoints.create(ClavaNodes.normalize(getNode().getSubExpr()), AExpression.class);
     }
 
     @Override
     public Boolean getIsPointerDerefImpl() {
-        return unaryOp.getOp() == UnaryOperatorKind.Deref;
+        return getNode().getOp() == UnaryOperatorKind.Deref;
     }
 
     @Override
@@ -55,7 +54,7 @@ public class CxxUnaryOp extends AUnaryOp {
 
     @Override
     public Boolean getIsBitwiseImpl() {
-        return unaryOp.getOp().isBitwise();
+        return getNode().getOp().isBitwise();
     }
 
 }
