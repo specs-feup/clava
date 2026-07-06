beca0bdb623a82bd13b287a85bd0871ce3d2b3bc
L. Sousa <lm.sousa@fe.up.pt>
2026-07-06 18:43:15 +0100

    Add aggregate Clava build and JS coverage
    Introduce a root Clava Gradle aggregate for Java and Clava-JS build/test tasks, validate the Clava-JS java-binaries symlink against ClavaWeaver installDist, and wire Clava-JS Jest runs to JaCoCo so Node-hosted JVM coverage is merged with Java test coverage.

--- Clava-JS/api/jest.config.ts
+++ Clava-JS/api/jest.config.ts
@@ -4,8 +4,8 @@ import { weaverConfig } from "../code/WeaverConfiguration.ts";
 export default defineConfig({
   preset: "ts-jest/presets/default-esm",
   testEnvironment: "@specs-feup/lara/jest/jestEnvironment.ts",
-  globalSetup: "@specs-feup/lara/jest/jestGlobalSetup.ts",
-  globalTeardown: "@specs-feup/lara/jest/jestGlobalTeardown.ts",
+  globalSetup: "<rootDir>/../jest/jestGlobalSetup.ts",
+  globalTeardown: "<rootDir>/../jest/jestGlobalTeardown.ts",
   setupFiles: ["@specs-feup/lara/jest/setupFiles/sharedJavaModule.ts"],
   setupFilesAfterEnv: ["@specs-feup/lara/jest/setupFiles/importSideEffects.ts"],
   moduleNameMapper: {
