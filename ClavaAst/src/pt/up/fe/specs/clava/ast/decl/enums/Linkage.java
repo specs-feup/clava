50a5d3850d8b5c33b25feadee32245db0b137d76
João Bispo <jbispo@fe.up.pt>
2026-08-13 18:00:57 +0100

    [ClavaAst] Adds value Linkage.None

--- ClavaAst/src/pt/up/fe/specs/clava/ast/decl/enums/Linkage.java
+++ ClavaAst/src/pt/up/fe/specs/clava/ast/decl/enums/Linkage.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2018 SPeCS.
- * 
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * <p>
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -15,13 +15,15 @@ package pt.up.fe.specs.clava.ast.decl.enums;
 
 /**
  * Describes the different kinds of linkage (C++ [basic.link], C99 6.2.2) that an entity may have.
- * 
+ *
  * (from Clang documentation)
- * 
+ *
  * @author JoaoBispo
  *
  */
 public enum Linkage {
+
+    None,
 
     /**
      * The entity is unique and can only be referred to from within its scope.
@@ -35,7 +37,7 @@ public enum Linkage {
 
     /**
      * External linkage within a unique namespace.
-     * 
+     *
      * <p>
      * From the language perspective, these entities have external linkage. However, since they reside in an anonymous
      * namespace, their names are unique to this translation unit, which is equivalent to having internal linkage from
@@ -52,7 +54,7 @@ public enum Linkage {
     /**
      * Internal linkage according to the Modules TS, but can be referred to from other translation units indirectly
      * through inline functions and templates in the module interface.
-     * 
+     *
      */
     ModuleInternalLinkage,
 
