972366571e18a8d5ea156c1db872ffaea0c11682
L. Sousa <lm.sousa@fe.up.pt>
2026-09-02 23:40:44 +0100

    Manual massaging of the vitest migration

--- Clava-JS/vitest.config.ts
+++ Clava-JS/vitest.config.ts
@@ -1,6 +1,4 @@
 import { createWeaverVitestConfig } from "@specs-feup/lara/vitest/weaverVitestConfig.ts";
 import { weaverConfig } from "./code/WeaverConfiguration.ts";
 
-export default createWeaverVitestConfig(weaverConfig, {
-  javaOptionsEnvironmentVariable: "CLAVA_JS_JAVA_OPTIONS",
-});
+export default createWeaverVitestConfig(weaverConfig);
