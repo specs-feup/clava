3c6dd39c832853bf1713a05417eaa04ed89d9f1a
L. Sousa <lm.sousa@fe.up.pt>
2026-09-07 16:14:03 +0100

    refactor(ClavaAst): delegate ClassesService to jOptions treenode ClassesService
    Keep the Clava-specific pieces: the Clang dump classname suffix mapping
    (Decl/Stmt/Type/Attr/OMP*/default -> pt.up.fe.specs.clava.ast packages,
    via customSimpleNameToFullName) and the Attr -> Attribute and OMP ->
    GenericClangOMP fallbacks (applied by overriding getClass, since the
    generic service only supports a single default class). The custom
    classname mapper check and the getClavaNodeBuilder public API are kept as
    thin wrappers. Reflection lookup order and results are unchanged.

--- ClavaAst/src/pt/up/fe/specs/clava/utils/ClassesService.java
+++ ClavaAst/src/pt/up/fe/specs/clava/utils/ClassesService.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2018 SPeCS.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -13,8 +13,7 @@
 
 package pt.up.fe.specs.clava.utils;
 
-import java.lang.reflect.Constructor;
-import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,26 +27,30 @@ import pt.up.fe.specs.clava.ClavaLog;
 import pt.up.fe.specs.clava.ClavaNode;
 import pt.up.fe.specs.clava.ast.attr.Attribute;
 import pt.up.fe.specs.clava.ast.omp.clang.GenericClangOMP;
-import pt.up.fe.specs.util.SpecsLogs;
 
-public class ClassesService {
+/**
+ * Maps Clang AST dump classnames to ClavaNode classes.
+ *
+ * <p>
+ * Delegates the generic lookup machinery to jOptions' ClassesService, keeping only the Clava-specific rules: the
+ * classname suffix mapping to the pt.up.fe.specs.clava.ast packages, and the two fallbacks for unknown attributes and
+ * OMP nodes.
+ */
+public class ClassesService extends org.suikasoft.jOptions.treenode.ClassesService<ClavaNode> {
 
     private static final String CLAVA_AST_PACKAGE = "pt.up.fe.specs.clava.ast";
     private static final DataStore EMPTY_DATA_STORE = DataStore.newInstance("Empty DataStore");
 
     private static final ClassesService STATIC_INSTANCE = new ClassesService();
 
-    private static final Set<String> WARNED_CLASSES = ConcurrentHashMap.newKeySet();
-
     private final CustomClassnameMapper customClassMap;
-    private final Map<String, Class<? extends ClavaNode>> autoClassMap;
-    // private final Map<String, String> warnedClassnames;
+    private final Map<String, Class<? extends ClavaNode>> fallbackClassMap = new HashMap<>();
+    private final Set<String> warnedClasses = ConcurrentHashMap.newKeySet();
 
     public ClassesService(CustomClassnameMapper customClassMap) {
+        // The suffix mapping below always produces a full name, so no package scan is needed
+        super(ClavaNode.class, Collections.emptyList());
         this.customClassMap = customClassMap;
-        this.autoClassMap = new HashMap<>();
-        // this.warnedClassnames = new HashSet<>();
-        // this.warnedClassnames = new ConcurrentHashMap<>();
     }
 
     public ClassesService() {
@@ -62,15 +65,6 @@ public class ClassesService {
         return STATIC_INSTANCE.getClass(classname, EMPTY_DATA_STORE);
     }
 
-    // public void addCustomMapping(String className, Class<? extends ClavaNode> clavaNodeClass) {
-    // autoClassMap.put(className, clavaNodeClass);
-    // }
-
-    // public Class<? extends ClavaNode> getClass(String classname) {
-    // // TODO: Maybe DataStore argument is no longer needed? It is required only for custom mappings
-    // return getClass(classname, EMPTY_DATA_STORE);
-    // }
-
     public Class<? extends ClavaNode> getClass(String classname, DataStore data) {
 
         // Try custom map
@@ -79,57 +73,56 @@ public class ClassesService {
             return clavaNodeClass;
         }
 
-        // Try cached nodes
-        clavaNodeClass = autoClassMap.get(classname);
-        if (clavaNodeClass != null) {
-            return clavaNodeClass;
-        }
-
-        // Try discovering the node
-        clavaNodeClass = discoverClass(classname);
-        autoClassMap.put(classname, clavaNodeClass);
-        return clavaNodeClass;
+        return getClass(classname);
     }
 
-    // private Optional<Class<? extends ClavaNode>> discoverClass(String clangClassname) {
-    private Class<? extends ClavaNode> discoverClass(String clangClassname) {
+    @Override
+    public Class<? extends ClavaNode> getClass(String classname) {
 
-        String fullClassname = simpleNameToFullName(clangClassname);
+        // Try cached fallbacks
+        var fallbackClass = fallbackClassMap.get(classname);
+        if (fallbackClass != null) {
+            return fallbackClass;
+        }
 
         try {
-            // Get class
-            Class<?> aClass = Class.forName(fullClassname);
-
-            // Check if class is a subtype of ClavaNode
-            if (!ClavaNode.class.isAssignableFrom(aClass)) {
-                throw new RuntimeException("Classname '" + clangClassname + "' was converted to a (" + fullClassname
-                        + ") that is not a ClavaNode");
+            return super.getClass(classname);
+        } catch (RuntimeException e) {
+            var newFallbackClass = discoverFallbackClass(classname);
+            if (newFallbackClass == null) {
+                throw e;
             }
 
-            // Cast class object
-            // return Optional.of(aClass.asSubclass(ClavaNode.class));
-            return aClass.asSubclass(ClavaNode.class);
-
-        } catch (ClassNotFoundException e) {
-
-            if (clangClassname.endsWith("Attr")) {
-                if (!WARNED_CLASSES.contains(clangClassname)) {
-                    WARNED_CLASSES.add(clangClassname);
-
-                    ClavaLog.info("No parser defined for attribute '" + clangClassname
-                            + "', using generic attribute parser with no arguments");
-                }
-
-                return Attribute.class;
-            }
-
-            if (clangClassname.startsWith("OMP")) {
-                return GenericClangOMP.class;
-            }
-
-            throw new RuntimeException("Could not map classname '" + clangClassname + "' to a ClavaNode class");
-            // return Optional.empty();
+            fallbackClassMap.put(classname, newFallbackClass);
+            return newFallbackClass;
         }
+    }
+
+    /**
+     * Clava-specific fallbacks, applied when no class is found for the given classname.
+     */
+    private Class<? extends ClavaNode> discoverFallbackClass(String classname) {
+        if (classname.endsWith("Attr")) {
+            if (!warnedClasses.contains(classname)) {
+                warnedClasses.add(classname);
+
+                ClavaLog.info("No parser defined for attribute '" + classname
+                        + "', using generic attribute parser with no arguments");
+            }
+
+            return Attribute.class;
+        }
+
+        if (classname.startsWith("OMP")) {
+            return GenericClangOMP.class;
+        }
+
+        return null;
+    }
+
+    @Override
+    protected String customSimpleNameToFullName(String nodeClassname) {
+        return simpleNameToFullName(nodeClassname);
     }
 
     private static String simpleNameToFullName(String nodeClassname) {
@@ -159,26 +152,7 @@ public class ClassesService {
 
     public BiFunction<DataStore, List<? extends ClavaNode>, ClavaNode> getClavaNodeBuilder(
             Class<? extends ClavaNode> clavaNodeClass) {
-
-        // Create builder
-        try {
-
-            Constructor<? extends ClavaNode> constructor = clavaNodeClass.getConstructor(DataStore.class,
-                    Collection.class);
-
-            return (data, children) -> {
-                try {
-                    return constructor.newInstance(data, children);
-                } catch (Exception e) {
-                    throw new RuntimeException("Could not call constructor for ClavaNode", e);
-                }
-            };
-
-        } catch (Exception e) {
-            SpecsLogs.msgLib("Could not create constructor for ClavaNode:" + e.getMessage());
-            return null;
-        }
-
+        return getNodeBuilder(clavaNodeClass);
     }
 
 }
